43f1e67dd19ff9d0d04a33de31778e9cb4c07677
rapon1kt <shinnatensei@gmail.com>
2025-08-04 18:49:21 -0300

    feat: implementing basic auth with user and password only to verify requests

---
+++ src/main/java/com/raponi/blog/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.raponi.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  public @Bean SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(customizer -> customizer.disable())
+        .authorizeHttpRequests((authorize) -> authorize
+            .anyRequest().authenticated())
+        .httpBasic(Customizer.withDefaults());
+
+    return http.build();
+  }
+
+  public @Bean PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  public @Bean UserDetailsService userDetailsService() {
+    UserDetails userDetails = User.builder()
+        .username("user")
+        .password(passwordEncoder().encode("password"))
+        .roles("USER")
+        .build();
+    return new InMemoryUserDetailsManager(userDetails);
+  }
+}
