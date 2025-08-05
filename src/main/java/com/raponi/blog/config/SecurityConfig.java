c3db05330a81deefd37c56747b1325069d823974
rapon1kt <shinnatensei@gmail.com>
2025-08-05 11:38:45 -0300

    refactor: implementation of automatic registration with password encryption

--- src/main/java/com/raponi/blog/config/SecurityConfig.java
+++ src/main/java/com/raponi/blog/config/SecurityConfig.java
@@ -1,27 +1,43 @@
 package com.raponi.blog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.raponi.blog.service.AppAccountService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+  @Autowired
+  private final AppAccountService appAccountService;
+
+  public SecurityConfig(AppAccountService appAccountService) {
+    this.appAccountService = appAccountService;
+  }
+
   public @Bean SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
-        .csrf(customizer -> customizer.disable())
-        .authorizeHttpRequests((authorize) -> authorize
-            .anyRequest().authenticated())
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(registry -> {
+          registry.requestMatchers("/req/**", "/css/**", "/js/**").permitAll();
+          registry.anyRequest().authenticated();
+        })
+        .formLogin(httpForm -> {
+          httpForm.loginPage("/req/login").permitAll();
+          httpForm.defaultSuccessUrl("/index");
+        })
         .httpBasic(Customizer.withDefaults());
 
     return http.build();
@@ -32,11 +48,13 @@ public class SecurityConfig {
   }
 
   public @Bean UserDetailsService userDetailsService() {
-    UserDetails userDetails = User.builder()
-        .username("user")
-        .password(passwordEncoder().encode("password"))
-        .roles("USER")
-        .build();
-    return new InMemoryUserDetailsManager(userDetails);
+    return appAccountService;
   }
+
+  public @Bean AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider(appAccountService);
+    provider.setPasswordEncoder(passwordEncoder());
+    return provider;
+  }
+
 }
