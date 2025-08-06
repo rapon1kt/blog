a8643aa703de32e546367d3bc5af937da5abd1be
rapon1kt <shinnatensei@gmail.com>
2025-08-06 18:15:14 -0300

    refactor: removing external dependencies of the model

--- src/main/java/com/raponi/blog/domain/model/Account.java
+++ src/main/java/com/raponi/blog/domain/model/Account.java
@@ -2,20 +2,11 @@ package com.raponi.blog.domain.model;
 
 import java.time.Instant;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import lombok.Data;
-
-@Data
-@Document(collection = "accounts")
-public class Account {
-  @Id
-  private String id;
-  private String email;
-  private String username;
-  private String password;
-
-  private Instant createdAt;
-  private Instant modifiedAt;
+public record Account(
+    String id,
+    String email,
+    String username,
+    String password,
+    Instant createdAt,
+    Instant modifiedAt) {
 }
