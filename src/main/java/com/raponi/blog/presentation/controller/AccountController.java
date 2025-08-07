46ce71f197839fa39b813143f504a6aa257a8bf5
rapon1kt <shinnatensei@gmail.com>
2025-08-07 15:41:55 -0300

    refactor: changing service implementation on AccountController

--- src/main/java/com/raponi/blog/presentation/controller/AccountController.java
+++ src/main/java/com/raponi/blog/presentation/controller/AccountController.java
@@ -3,7 +3,6 @@ package com.raponi.blog.presentation.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,35 +11,48 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.raponi.blog.application.service.AccountServiceImpl;
+import com.raponi.blog.application.service.account.DeleteAccountService;
+import com.raponi.blog.application.service.account.FindAccountByIdService;
+import com.raponi.blog.application.service.account.FindAllAccountsService;
+import com.raponi.blog.application.service.account.UpdateAccountService;
 import com.raponi.blog.domain.model.Account;
 
 @RestController
 @RequestMapping("/accounts")
 public class AccountController {
 
-  @Autowired
-  public AccountServiceImpl accountServiceImpl;
+  public FindAllAccountsService findAllService;
+  public FindAccountByIdService findAccountService;
+  public UpdateAccountService updateAccountService;
+  public DeleteAccountService deleteAccountService;
+
+  public AccountController(FindAllAccountsService findAllService, FindAccountByIdService findAccountService,
+      UpdateAccountService updateAccountService, DeleteAccountService deleteAccountService) {
+    this.findAllService = findAllService;
+    this.findAccountService = findAccountService;
+    this.updateAccountService = updateAccountService;
+    this.deleteAccountService = deleteAccountService;
+  }
 
   @GetMapping("/")
   public List<Account> getAllAccounts() {
-    return this.accountServiceImpl.getAllAccounts();
+    return this.findAllService.handle();
   }
 
   @GetMapping("/{id}")
   public Optional<Account> getAccountById(@PathVariable("id") String accountId) {
-    return this.accountServiceImpl.getAccountById(accountId);
+    return this.findAccountService.handle(accountId);
   }
 
   @PutMapping("/{id}")
   public Account updateAccountById(@PathVariable("id") String accountId,
       @RequestBody Account newAccountInfos) {
-    return this.accountServiceImpl.updateAccountById(accountId, newAccountInfos);
+    return this.updateAccountService.handle(accountId, newAccountInfos);
   }
 
   @DeleteMapping("/{id}")
   public String deleteAccountById(@PathVariable("id") String accountId) {
-    return this.accountServiceImpl.deleteAccountById(accountId);
+    return this.deleteAccountService.handle(accountId);
   }
 
 }
