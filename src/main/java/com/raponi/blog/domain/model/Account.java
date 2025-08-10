70a7324fb944fc2da634a82093aa4bd54e7b144b
rapon1kt <shinnatensei@gmail.com>
2025-08-09 23:15:10 -0300

    refactor: implementing create and update method to Account record

--- src/main/java/com/raponi/blog/domain/model/Account.java
+++ src/main/java/com/raponi/blog/domain/model/Account.java
@@ -9,4 +9,25 @@ public record Account(
     String password,
     Instant createdAt,
     Instant modifiedAt) {
+
+  public static Account create(String email, String username, String hashedPassword) {
+    Instant instNow = Instant.now();
+    return new Account(
+        null,
+        email,
+        username,
+        hashedPassword,
+        instNow,
+        instNow);
+  }
+
+  public Account update(String newUsername) {
+    return new Account(
+        id,
+        email,
+        newUsername,
+        password,
+        createdAt,
+        Instant.now());
+  }
 }
