4304fa216deb5c66d7f9a010c6fc914cbf86c608
rapon1kt <shinnatensei@gmail.com>
2025-08-12 11:16:43 -0300

    refactor: change createAccountUseCase parameter

--- src/main/java/com/raponi/blog/domain/usecase/account/CreateAccountUseCase.java
+++ src/main/java/com/raponi/blog/domain/usecase/account/CreateAccountUseCase.java
@@ -1,7 +1,8 @@
 package com.raponi.blog.domain.usecase.account;
 
 import com.raponi.blog.domain.model.Account;
+import com.raponi.blog.presentation.protocols.Http;
 
 public interface CreateAccountUseCase {
-  public Account handle(Account newAccount);
+  public Account handle(Http.Body bodyRequest);
 }
