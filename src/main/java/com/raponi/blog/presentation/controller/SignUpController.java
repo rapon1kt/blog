80fbf37b4618cb2dad30cca150cd75fa3b35c8cf
rapon1kt <shinnatensei@gmail.com>
2025-08-12 11:15:46 -0300

    refactor: implementing custom request and response with helper and http protocol

--- src/main/java/com/raponi/blog/presentation/controller/SignUpController.java
+++ src/main/java/com/raponi/blog/presentation/controller/SignUpController.java
@@ -1,10 +1,12 @@
 package com.raponi.blog.presentation.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.raponi.blog.application.service.account.CreateAccountService;
-import com.raponi.blog.domain.model.Account;
+import com.raponi.blog.presentation.helpers.HttpHelper;
+import com.raponi.blog.presentation.protocols.Http;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,8 +18,12 @@ public class SignUpController {
   private CreateAccountService createAccountService;
 
   @PostMapping(value = "/req/signup", consumes = "application/json")
-  public Account postMethodName(@RequestBody Account account) {
-    return this.createAccountService.handle(account);
+  public ResponseEntity<?> signUp(@RequestBody Http.Body requestBody) {
+    try {
+      return HttpHelper.ok(this.createAccountService.handle(requestBody));
+    } catch (Exception e) {
+      return HttpHelper.badRequest(e);
+    }
   }
 
 }
