78151c2b318483b9e1c8e65daeba36a0c7e8e87b
rapon1kt <shinnatensei@gmail.com>
2025-08-13 17:01:43 -0300

    refactor: organizing accountController code and changing path of requests (implementing requestParam)

--- src/main/java/com/raponi/blog/presentation/controller/AccountController.java
+++ src/main/java/com/raponi/blog/presentation/controller/AccountController.java
@@ -3,7 +3,6 @@ package com.raponi.blog.presentation.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +13,7 @@ import com.raponi.blog.application.service.account.ChangeAccountPasswordService;
 import com.raponi.blog.application.service.account.DeleteAccountService;
 import com.raponi.blog.application.service.account.FindAccountByEmailService;
 import com.raponi.blog.application.service.account.FindAccountByIdService;
+import com.raponi.blog.application.service.account.FindAccountByUsernameService;
 import com.raponi.blog.application.service.account.FindAllAccountsService;
 import com.raponi.blog.application.service.account.UpdateAccountService;
 import com.raponi.blog.domain.model.Account;
@@ -26,23 +26,26 @@ public class AccountController {
   public FindAllAccountsService findAllService;
   public FindAccountByIdService findAccountByIdService;
   public FindAccountByEmailService findAccountByEmailService;
+  public FindAccountByUsernameService findAccountByUsernameService;
   public UpdateAccountService updateAccountService;
   public DeleteAccountService deleteAccountService;
   public ChangeAccountPasswordService changeAccountPasswordService;
 
   public AccountController(FindAllAccountsService findAllService, FindAccountByIdService findAccountByIdService,
       FindAccountByEmailService findAccountByEmailService,
+      FindAccountByUsernameService findAccountByUsernameService,
       UpdateAccountService updateAccountService, DeleteAccountService deleteAccountService,
       ChangeAccountPasswordService changeAccountPasswordService) {
     this.findAllService = findAllService;
     this.findAccountByIdService = findAccountByIdService;
     this.findAccountByEmailService = findAccountByEmailService;
+    this.findAccountByUsernameService = findAccountByUsernameService;
     this.updateAccountService = updateAccountService;
     this.deleteAccountService = deleteAccountService;
     this.changeAccountPasswordService = changeAccountPasswordService;
   }
 
-  @GetMapping("/")
+  @GetMapping
   public ResponseEntity<?> getAllAccounts() {
     try {
       return HttpHelper.ok(this.findAllService.handle());
@@ -51,8 +54,8 @@ public class AccountController {
     }
   }
 
-  @GetMapping("/{id}")
-  public ResponseEntity<?> getAccountById(@PathVariable("id") String accountId) {
+  @GetMapping("/id")
+  public ResponseEntity<?> getAccountById(@RequestParam("target") String accountId) {
     try {
       return HttpHelper.ok(this.findAccountByIdService.handle(accountId));
     } catch (Exception e) {
@@ -60,17 +63,8 @@ public class AccountController {
     }
   }
 
-  @GetMapping
-  public ResponseEntity<?> getAccountByEmail(@RequestParam("email") String email) {
-    try {
-      return HttpHelper.ok(this.findAccountByEmailService.handle(email));
-    } catch (Exception e) {
-      return HttpHelper.badRequest(e);
-    }
-  }
-
-  @PutMapping("/{id}")
-  public ResponseEntity<?> updateAccountById(@PathVariable("id") String accountId,
+  @PutMapping("/id")
+  public ResponseEntity<?> updateAccountById(@RequestParam("target") String accountId,
       @RequestBody Account newAccountInfos) {
     try {
       return HttpHelper.ok(this.updateAccountService.handle(accountId, newAccountInfos.username()));
@@ -79,20 +73,38 @@ public class AccountController {
     }
   }
 
-  @PutMapping("/newpassword/{id}")
-  public ResponseEntity<?> changeAccountPassword(@PathVariable("id") String accountId,
-      @RequestBody Account accountInfos) {
+  @DeleteMapping("/id")
+  public ResponseEntity<?> deleteAccountById(@RequestParam("target") String accountId) {
     try {
-      return HttpHelper.ok(this.changeAccountPasswordService.handle(accountId, accountInfos.password()));
+      return HttpHelper.ok(this.deleteAccountService.handle(accountId));
     } catch (Exception e) {
       return HttpHelper.badRequest(e);
     }
   }
 
-  @DeleteMapping("/{id}")
-  public ResponseEntity<?> deleteAccountById(@PathVariable("id") String accountId) {
+  @GetMapping("/email")
+  public ResponseEntity<?> getAccountByEmail(@RequestParam("target") String email) {
     try {
-      return HttpHelper.ok(this.deleteAccountService.handle(accountId));
+      return HttpHelper.ok(this.findAccountByEmailService.handle(email));
+    } catch (Exception e) {
+      return HttpHelper.badRequest(e);
+    }
+  }
+
+  @GetMapping("/username")
+  public ResponseEntity<?> getAccountByUsername(@RequestParam("target") String username) {
+    try {
+      return HttpHelper.ok(this.findAccountByUsernameService.handle(username));
+    } catch (Exception e) {
+      return HttpHelper.badRequest(e);
+    }
+  }
+
+  @PutMapping("/newpassword")
+  public ResponseEntity<?> changeAccountPassword(@RequestParam("target") String accountId,
+      @RequestBody Account accountInfos) {
+    try {
+      return HttpHelper.ok(this.changeAccountPasswordService.handle(accountId, accountInfos.password()));
     } catch (Exception e) {
       return HttpHelper.badRequest(e);
     }
