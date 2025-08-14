20309e161caac709d9f1a98f8e37ba6a96e720db
rapon1kt <shinnatensei@gmail.com>
2025-08-14 14:53:28 -0300

    refactor: separate Http protocol in registerBody and loginBody

--- src/main/java/com/raponi/blog/presentation/protocols/Http.java
+++ src/main/java/com/raponi/blog/presentation/protocols/Http.java
@@ -1,8 +1,13 @@
 package com.raponi.blog.presentation.protocols;
 
-public record Http(Body body) {
-  public record Body(
+public record Http(RegisterBody registerBody, LoginBody loginBody) {
+  public record RegisterBody(
       String email,
+      String username,
+      String password) {
+  }
+
+  public record LoginBody(
       String username,
       String password) {
   }
