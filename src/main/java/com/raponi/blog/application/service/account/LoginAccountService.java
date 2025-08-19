c1c0d0e8c795d769fd7deb4847d96736f422632c
rapon1kt <shinnatensei@gmail.com>
2025-08-19 15:01:27 -0300

    refactor: get accountId from requestBody and generateToken with accountId

--- src/main/java/com/raponi/blog/application/service/account/LoginAccountService.java
+++ src/main/java/com/raponi/blog/application/service/account/LoginAccountService.java
@@ -7,16 +7,20 @@ import org.springframework.stereotype.Service;
 
 import com.raponi.blog.application.service.JWTService;
 import com.raponi.blog.domain.usecase.account.LoginAccountUseCase;
+import com.raponi.blog.infrastructure.persistence.repository.AccountRepository;
 import com.raponi.blog.presentation.errors.AccountNotFound;
 import com.raponi.blog.presentation.protocols.Http;
 
 @Service
 public class LoginAccountService implements LoginAccountUseCase {
 
+  private final AccountRepository accountRepository;
   private final AuthenticationManager authenticationManager;
   private final JWTService jwtService;
 
-  public LoginAccountService(AuthenticationManager authenticationManager, JWTService jwtService) {
+  public LoginAccountService(AccountRepository accountRepository, AuthenticationManager authenticationManager,
+      JWTService jwtService) {
+    this.accountRepository = accountRepository;
     this.authenticationManager = authenticationManager;
     this.jwtService = jwtService;
   }
@@ -27,7 +31,8 @@ public class LoginAccountService implements LoginAccountUseCase {
         .authenticate(new UsernamePasswordAuthenticationToken(bodyRequest.username(), bodyRequest.password()));
 
     if (authentication.isAuthenticated()) {
-      return jwtService.generateToken(bodyRequest.username());
+      String accountId = this.accountRepository.findByUsername(bodyRequest.username()).get().id();
+      return jwtService.generateToken(bodyRequest.username(), accountId);
     }
 
     throw new AccountNotFound("this login");
