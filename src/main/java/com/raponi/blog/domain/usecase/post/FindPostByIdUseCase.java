65894aff84403ae473cff56ab56d710dd746c4e2
rapon1kt <shinnatensei@gmail.com>
2025-08-19 17:05:42 -0300

    refactor: add accountId to FindPostById use case handle method

--- src/main/java/com/raponi/blog/domain/usecase/post/FindPostByIdUseCase.java
+++ src/main/java/com/raponi/blog/domain/usecase/post/FindPostByIdUseCase.java
@@ -3,5 +3,5 @@ package com.raponi.blog.domain.usecase.post;
 import com.raponi.blog.domain.model.Post;
 
 public interface FindPostByIdUseCase {
-  public Post handle(String postId);
+  public Post handle(String postId, String accountId);
 }
