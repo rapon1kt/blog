358b9f745f7253ac2eb707283e389054b1002172
rapon1kt <shinnatensei@gmail.com>
2025-08-19 17:27:19 -0300

    refactor: fixing logic in findPostByIdService

--- src/main/java/com/raponi/blog/application/service/posts/FindPostByIdService.java
+++ src/main/java/com/raponi/blog/application/service/posts/FindPostByIdService.java
@@ -21,10 +21,13 @@ public class FindPostByIdService implements FindPostByIdUseCase {
   public Post handle(String postId, String accountId) {
     Optional<Post> post = this.postRepository.findById(postId);
     if (post.isPresent()) {
-      if (post.get().privateStatus() && post.get().accountId().equals(accountId)) {
-        return this.postRepository.findById(postId).get();
+      if (post.get().privateStatus()) {
+        if (post.get().accountId().equals(accountId)) {
+          return this.postRepository.findById(postId).get();
+        }
+        throw new IllegalArgumentException("Você não tem permissão para visualizar esse post.");
       }
-      throw new IllegalArgumentException("Você não tem permissão para visualizar esse post.");
+      return this.postRepository.findById(postId).get();
     }
     throw new IllegalArgumentException("Post não encontrado ou não existe.");
   }
