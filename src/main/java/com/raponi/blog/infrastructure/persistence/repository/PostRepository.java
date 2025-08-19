00f40e4e0c75b347ac47205b92c0cf73a1c7cc0c
rapon1kt <shinnatensei@gmail.com>
2025-08-18 21:59:03 -0300

    feat: creating post repository and implementing some custom methods

---
+++ src/main/java/com/raponi/blog/infrastructure/persistence/repository/PostRepository.java
@@ -0,0 +1,15 @@
+package com.raponi.blog.infrastructure.persistence.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.raponi.blog.domain.model.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+  List<Post> findByAccountId(String accountId);
+
+  List<Post> findByPrivateStatus(boolean privateStatus);
+}
