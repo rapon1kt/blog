810cd1eecc517dcac77226ceab8842f995d50f79
rapon1kt <shinnatensei@gmail.com>
2025-08-19 17:34:19 -0300

    feat: add tokenId to updatePostStatus usecase handle method

--- src/main/java/com/raponi/blog/domain/usecase/post/UpdatePostStatusUseCase.java
+++ src/main/java/com/raponi/blog/domain/usecase/post/UpdatePostStatusUseCase.java
@@ -3,5 +3,5 @@ package com.raponi.blog.domain.usecase.post;
 import com.raponi.blog.domain.model.Post;
 
 public interface UpdatePostStatusUseCase {
-  public Post handle(String accountId, String postId);
+  public Post handle(String accountId, String postId, String tokenId);
 }
