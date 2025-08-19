98314d01eafe140e3d11904a9a7977d530224011
rapon1kt <shinnatensei@gmail.com>
2025-08-19 17:33:20 -0300

    feat: add tokenId to deletePost usecase handle method

--- src/main/java/com/raponi/blog/domain/usecase/post/DeletePostUseCase.java
+++ src/main/java/com/raponi/blog/domain/usecase/post/DeletePostUseCase.java
@@ -1,5 +1,5 @@
 package com.raponi.blog.domain.usecase.post;
 
 public interface DeletePostUseCase {
-  public String handle(String accountId, String postId);
+  public String handle(String accountId, String postId, String tokenId);
 }
