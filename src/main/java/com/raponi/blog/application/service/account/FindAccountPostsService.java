7d0ca4542422d14740c70ee7e598c6ffb1f7a4c8
rapon1kt <shinnatensei@gmail.com>
2025-08-19 16:00:17 -0300

    feat: implementing privateStatus logic with currentAccountId and account verification

--- src/main/java/com/raponi/blog/application/service/account/FindAccountPostsService.java
+++ src/main/java/com/raponi/blog/application/service/account/FindAccountPostsService.java
@@ -1,25 +1,37 @@
 package com.raponi.blog.application.service.account;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.raponi.blog.domain.model.Account;
 import com.raponi.blog.domain.model.Post;
 import com.raponi.blog.domain.usecase.account.FindAccountPostsUseCase;
+import com.raponi.blog.infrastructure.persistence.repository.AccountRepository;
 import com.raponi.blog.infrastructure.persistence.repository.PostRepository;
 
 @Service
 public class FindAccountPostsService implements FindAccountPostsUseCase {
 
   private final PostRepository postRepository;
+  private final AccountRepository accountRepository;
 
-  public FindAccountPostsService(PostRepository postRepository) {
+  public FindAccountPostsService(PostRepository postRepository, AccountRepository accountRepository) {
     this.postRepository = postRepository;
+    this.accountRepository = accountRepository;
   }
 
   @Override
-  public List<Post> handle(String accountId) {
-    return this.postRepository.findByAccountId(accountId);
+  public List<Post> handle(String accountId, String currentAccountId) {
+    Optional<Account> account = this.accountRepository.findById(accountId);
+    if (account.isPresent()) {
+      if (accountId.equals(currentAccountId)) {
+        return this.postRepository.findByAccountId(accountId);
+      }
+      return this.postRepository.findByAccountIdAndPrivateStatusFalse(accountId);
+    }
+    throw new IllegalArgumentException("O usuário em questão não existe.");
   }
 
 }
