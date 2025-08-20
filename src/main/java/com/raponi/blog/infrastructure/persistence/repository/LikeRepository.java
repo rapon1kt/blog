a67d665a81ff05033134858430c1d76e7a359347
rapon1kt <shinnatensei@gmail.com>
2025-08-20 18:06:05 -0300

    feat: creating LikeRepository with custom methods

---
+++ src/main/java/com/raponi/blog/infrastructure/persistence/repository/LikeRepository.java
@@ -0,0 +1,15 @@
+package com.raponi.blog.infrastructure.persistence.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.raponi.blog.domain.model.Like;
+
+@Repository
+public interface LikeRepository extends MongoRepository<Like, String> {
+  long countByPostId(String postId);
+
+  boolean existsByPostIdAndAccountId(String postId, String accountId);
+
+  void deleteByPostIdAndAccountId(String postId, String accountId);
+}
