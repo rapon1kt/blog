1d5ced04ea101b439a1d42bfd12e32cc9e97e740
rapon1kt <shinnatensei@gmail.com>
2025-08-21 09:29:34 -0300

    remove: param of changeStatus handle method

--- src/main/java/com/raponi/blog/domain/usecase/account/ChangeStatusUseCase.java
+++ src/main/java/com/raponi/blog/domain/usecase/account/ChangeStatusUseCase.java
@@ -3,5 +3,5 @@ package com.raponi.blog.domain.usecase.account;
 import com.raponi.blog.presentation.protocols.Http;
 
 public interface ChangeStatusUseCase {
-  public Http.ResponseBody handle(String accountId, boolean newActiveStatus);
+  public Http.ResponseBody handle(String accountId);
 }
