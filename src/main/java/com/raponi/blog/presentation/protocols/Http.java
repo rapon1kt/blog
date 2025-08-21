0d8bee24653b67f564fc77ddf0a22087682ff63b
rapon1kt <shinnatensei@gmail.com>
2025-08-21 09:27:12 -0300

    remove: updateBody from httpProtocol

--- src/main/java/com/raponi/blog/presentation/protocols/Http.java
+++ src/main/java/com/raponi/blog/presentation/protocols/Http.java
@@ -2,7 +2,7 @@ package com.raponi.blog.presentation.protocols;
 
 import java.time.Instant;
 
-public record Http(RegisterBody registerBody, LoginBody loginBody, UpdateBody updateBody, ResponseBody responseBody) {
+public record Http(RegisterBody registerBody, LoginBody loginBody, ResponseBody responseBody) {
   public record RegisterBody(
       String email,
       String username,
@@ -12,12 +12,6 @@ public record Http(RegisterBody registerBody, LoginBody loginBody, UpdateBody up
   public record LoginBody(
       String username,
       String password) {
-  }
-
-  public record UpdateBody(
-      String username,
-      String password,
-      boolean active) {
   }
 
   public record ResponseBody(
