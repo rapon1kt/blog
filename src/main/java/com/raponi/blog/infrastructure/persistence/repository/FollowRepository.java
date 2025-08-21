70f131d2cf3c3399ae7239a30dabbd3fea651fc1
rapon1kt <shinnatensei@gmail.com>
2025-08-21 09:33:18 -0300

    feat: implementing followRepository

---
+++ src/main/java/com/raponi/blog/infrastructure/persistence/repository/FollowRepository.java
@@ -0,0 +1,19 @@
+package com.raponi.blog.infrastructure.persistence.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.raponi.blog.domain.model.Follow;
+
+public interface FollowRepository extends MongoRepository<Follow, String> {
+
+  List<Follow> findByFollowerId(String followerId);
+
+  List<Follow> findByFollowingId(String followingId);
+
+  boolean existsByFollowerIdAndFollowingId(String followerId, String followingId);
+
+  void deleteByFollowerIdAndFollowingId(String followerId, String followingId);
+
+}
