7efe9ecf0633d1e7a630f3354278a6ab85983836
rapon1kt <shinnatensei@gmail.com>
2025-08-21 09:30:19 -0300

    remove: param of changeStatus and add logic to toggle activeStatus

--- src/main/java/com/raponi/blog/domain/model/Account.java
+++ src/main/java/com/raponi/blog/domain/model/Account.java
@@ -29,8 +29,8 @@ public record Account(
     return new Account(id, email, username, newPassword, active, createdAt, Instant.now());
   }
 
-  public Account changeStatus(boolean activeStatus) {
-    return new Account(id, email, username, password, activeStatus, createdAt, Instant.now());
+  public Account changeStatus() {
+    return new Account(id, email, username, password, active ? false : true, createdAt, Instant.now());
   }
 
   public Account update(String newUsername) {
