f3da2e30f24681c091001928b413fc8ff0342f8d
rapon1kt <shinnatensei@gmail.com>
2025-08-23 11:51:52 -0300

    feat: add validation of request account and ensure that everything linked to account is deleted

--- src/main/java/com/raponi/blog/application/service/account/DeleteAccountService.java
+++ src/main/java/com/raponi/blog/application/service/account/DeleteAccountService.java
@@ -1,31 +1,43 @@
 package com.raponi.blog.application.service.account;
 
-import java.util.Optional;
-
 import org.springframework.stereotype.Service;
 
+import com.raponi.blog.application.service.AccountValidatorService;
 import com.raponi.blog.domain.model.Account;
 import com.raponi.blog.domain.usecase.account.DeleteAccountUseCase;
 import com.raponi.blog.infrastructure.persistence.repository.AccountRepository;
-import com.raponi.blog.presentation.errors.AccountNotFound;
+import com.raponi.blog.infrastructure.persistence.repository.FollowRepository;
+import com.raponi.blog.infrastructure.persistence.repository.LikeRepository;
+import com.raponi.blog.infrastructure.persistence.repository.PostRepository;
 
 @Service
 public class DeleteAccountService implements DeleteAccountUseCase {
 
   private final AccountRepository accountRepository;
+  private final PostRepository postRepository;
+  private final LikeRepository likeRepository;
+  private final FollowRepository followRepository;
+  private final AccountValidatorService accountValidatorService;
 
-  public DeleteAccountService(AccountRepository accountRepository) {
+  public DeleteAccountService(AccountRepository accountRepository, PostRepository postRepository,
+      LikeRepository likeRepository, FollowRepository followRepository,
+      AccountValidatorService accountValidatorService) {
     this.accountRepository = accountRepository;
+    this.postRepository = postRepository;
+    this.likeRepository = likeRepository;
+    this.followRepository = followRepository;
+    this.accountValidatorService = accountValidatorService;
   }
 
   @Override
-  public String handle(String accountId) {
-    Optional<Account> account = this.accountRepository.findById(accountId);
-    if (account.isPresent()) {
-      this.accountRepository.deleteById(accountId);
-      return "Account with id equals " + accountId + " deleted with success";
-    }
-    throw new AccountNotFound("id equals " + accountId);
+  public String handle(String tokenId, String accountId, String role, String password) {
+    Account account = this.accountValidatorService.handle(tokenId, accountId, password, role);
+    this.accountRepository.deleteById(account.id());
+    this.postRepository.deleteByAccountId(account.id());
+    this.likeRepository.deleteByAccountId(account.id());
+    this.followRepository.deleteByFollowerId(account.id());
+    this.followRepository.deleteByFollowingId(account.id());
+    return "Account with id equals " + account.id() + " deleted with success";
   }
 
 }
