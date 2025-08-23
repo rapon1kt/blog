bfa0f34fd3804174f08d25a7a7dcd5a193784b17
rapon1kt <shinnatensei@gmail.com>
2025-08-23 10:45:40 -0300

    feat: create account validator service

---
+++ src/main/java/com/raponi/blog/application/service/AccountValidatorService.java
@@ -0,0 +1,43 @@
+package com.raponi.blog.application.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.raponi.blog.domain.model.Account;
+import com.raponi.blog.infrastructure.persistence.repository.AccountRepository;
+import com.raponi.blog.presentation.errors.AccountNotFound;
+
+@Service
+public class AccountValidatorService {
+
+  private final AccountRepository accountRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  public AccountValidatorService(AccountRepository accountRepository, PasswordEncoder passwordEncoder) {
+    this.accountRepository = accountRepository;
+    this.passwordEncoder = passwordEncoder;
+  }
+
+  public Account handle(String tokenId, String accountId, String password, String role) {
+    Account account = this.accountRepository.findById(accountId)
+        .orElseThrow(() -> new AccountNotFound("id equals " + accountId));
+
+    if (!accountId.equals(tokenId) && !isAdmin(role)) {
+      throw new IllegalArgumentException("Você não tem permissão para fazer isso.");
+    } else if (!account.active()) {
+      throw new IllegalArgumentException("Essa conta está desativada");
+    } else if (!passwordMatches(password, account.password())) {
+      throw new IllegalArgumentException("Sua senha está incorreta.");
+    }
+    return account;
+  }
+
+  public boolean isAdmin(String role) {
+    return role.equals("ROLE_ADMIN");
+  }
+
+  public boolean passwordMatches(String password, String hashedPassword) {
+    return this.passwordEncoder.matches(password, hashedPassword);
+  }
+
+}
