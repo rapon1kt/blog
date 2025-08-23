580319e69f1c6e5904972f91a62e86b759d63128
rapon1kt <shinnatensei@gmail.com>
2025-08-23 11:47:58 -0300

    refactor: add new body with password authorization

--- src/main/java/com/raponi/blog/presentation/protocols/Http.java
+++ src/main/java/com/raponi/blog/presentation/protocols/Http.java
@@ -3,7 +3,7 @@ package com.raponi.blog.presentation.protocols;
 import java.time.Instant;
 
 public record Http(RegisterBody registerBody, LoginBody loginBody, ResponseBody responseBody,
-    UpdateBody updateBody) {
+    UpdateBody updateBody, AuthBody authBody) {
   public record RegisterBody(
       String email,
       String username,
@@ -29,5 +29,10 @@ public record Http(RegisterBody registerBody, LoginBody loginBody, ResponseBody 
       String password,
       String newPassword,
       boolean active) {
+  }
+
+  public record AuthBody(
+      String password) {
+
   }
 }
