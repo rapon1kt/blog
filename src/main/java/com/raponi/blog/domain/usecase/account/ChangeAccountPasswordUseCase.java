5f6fd754f3c5b6f47e777349fa1f63c887c9c1df
rapon1kt <shinnatensei@gmail.com>
2025-08-23 10:52:03 -0300

    refactor: change response of passwordChange and add new params to verify security

--- src/main/java/com/raponi/blog/domain/usecase/account/ChangeAccountPasswordUseCase.java
+++ src/main/java/com/raponi/blog/domain/usecase/account/ChangeAccountPasswordUseCase.java
@@ -1,7 +1,5 @@
 package com.raponi.blog.domain.usecase.account;
 
-import com.raponi.blog.presentation.protocols.Http;
-
 public interface ChangeAccountPasswordUseCase {
-  public Http.ResponseBody handle(String accountId, String newPassword);
+  public String handle(String accountId, String role, String tokenId, String password, String newPassword);
 }
