d394dd1ad95705b05f48c0c1de9b2fb9e4335961
rapon1kt <shinnatensei@gmail.com>
2025-08-24 22:43:39 -0300

    feat: add verification to findAccountByEmailService

--- src/main/java/com/raponi/blog/application/service/account/FindAccountByEmailService.java
+++ src/main/java/com/raponi/blog/application/service/account/FindAccountByEmailService.java
@@ -1,31 +1,30 @@
 package com.raponi.blog.application.service.account;
 
-import java.util.Optional;
-
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import com.raponi.blog.application.service.AccountValidatorService;
 import com.raponi.blog.domain.model.Account;
 import com.raponi.blog.domain.usecase.account.FindAccountByEmailUseCase;
-import com.raponi.blog.infrastructure.persistence.repository.AccountRepository;
-import com.raponi.blog.presentation.errors.AccountNotFound;
 import com.raponi.blog.presentation.protocols.Http;
 
 @Service
 public class FindAccountByEmailService implements FindAccountByEmailUseCase {
 
-  private final AccountRepository accountRepository;
+  private final AccountValidatorService accountValidatorService;
 
-  public FindAccountByEmailService(AccountRepository accountRepository) {
-    this.accountRepository = accountRepository;
+  public FindAccountByEmailService(AccountValidatorService accountValidatorService) {
+    this.accountValidatorService = accountValidatorService;
   }
 
   @Override
   public Http.ResponseBody handle(String email) {
-    Optional<Account> account = this.accountRepository.findByEmail(email);
-    if (account.isPresent()) {
-      return account.get().toResponseBody();
-    }
-    throw new AccountNotFound("email equals " + email);
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    String tokenId = auth.getName();
+    String role = auth.getAuthorities().iterator().next().getAuthority();
+    Account acc = this.accountValidatorService.verifyWithEmailOrAccountId(null, tokenId, role, email);
+    return acc.toResponseBody();
   }
 
 }
