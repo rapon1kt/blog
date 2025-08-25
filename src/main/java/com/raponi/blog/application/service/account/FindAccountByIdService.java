fbc8122015ba8a4ba167f4b8b53ff929e68ecfe6
rapon1kt <shinnatensei@gmail.com>
2025-08-25 14:18:37 -0300

    feat: implementing validation of request in findAccountById

--- src/main/java/com/raponi/blog/application/service/account/FindAccountByIdService.java
+++ src/main/java/com/raponi/blog/application/service/account/FindAccountByIdService.java
@@ -1,31 +1,30 @@
 package com.raponi.blog.application.service.account;
 
-import java.util.Optional;
-
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import com.raponi.blog.application.service.AccountValidatorService;
 import com.raponi.blog.domain.model.Account;
 import com.raponi.blog.domain.usecase.account.FindAccountByIdUseCase;
-import com.raponi.blog.infrastructure.persistence.repository.AccountRepository;
-import com.raponi.blog.presentation.errors.AccountNotFound;
 import com.raponi.blog.presentation.protocols.Http;
 
 @Service
 public class FindAccountByIdService implements FindAccountByIdUseCase {
 
-  private final AccountRepository accountRepository;
+  private final AccountValidatorService accountValidatorService;
 
-  public FindAccountByIdService(AccountRepository accountRepository) {
-    this.accountRepository = accountRepository;
+  public FindAccountByIdService(AccountValidatorService accountValidatorService) {
+    this.accountValidatorService = accountValidatorService;
   }
 
   @Override
   public Http.ResponseBody handle(String accountId) {
-    Optional<Account> account = this.accountRepository.findById(accountId);
-    if (account.isPresent()) {
-      return account.get().toResponseBody();
-    }
-    throw new AccountNotFound("id equals " + accountId);
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    String tokenId = auth.getName();
+    String role = auth.getAuthorities().iterator().next().getAuthority();
+    Account account = this.accountValidatorService.getAccountByEmailOrAccountId(accountId, tokenId, role, null);
+    return account.toResponseBody();
   }
 
 }
