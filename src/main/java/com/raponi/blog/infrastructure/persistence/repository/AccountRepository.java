a22c38e8724cc44f012ac9e3000dde94aadc6027
rapon1kt <shinnatensei@gmail.com>
2025-08-25 14:16:10 -0300

    feat: implementing new query method to accountRepository

--- src/main/java/com/raponi/blog/infrastructure/persistence/repository/AccountRepository.java
+++ src/main/java/com/raponi/blog/infrastructure/persistence/repository/AccountRepository.java
@@ -3,6 +3,7 @@ package com.raponi.blog.infrastructure.persistence.repository;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.raponi.blog.domain.model.Account;
@@ -17,5 +18,8 @@ public interface AccountRepository extends MongoRepository<Account, String> {
   public Optional<Account> findByEmail(String email);
 
   boolean existsByEmail(String email);
+
+  @Query("{ '$or': [{ 'email': ?0 }, { '_id': ?1 }]}")
+  public Optional<Account> findByEmailOrId(String email, String id);
 
 }
