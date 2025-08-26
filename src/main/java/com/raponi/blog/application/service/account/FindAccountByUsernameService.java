36760cbbacb9b1fe8e9905ca8ffc1c8fe0f71b08
rapon1kt <shinnatensei@gmail.com>
2025-08-26 17:21:52 -0300

    feat: add verification to find account by username service

--- src/main/java/com/raponi/blog/application/service/account/FindAccountByUsernameService.java
+++ src/main/java/com/raponi/blog/application/service/account/FindAccountByUsernameService.java
@@ -2,30 +2,34 @@ package com.raponi.blog.application.service.account;
 
 import java.util.Optional;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import com.raponi.blog.application.service.AccountValidatorService;
 import com.raponi.blog.domain.model.Account;
 import com.raponi.blog.domain.usecase.account.FindAccountByUsernameUseCase;
 import com.raponi.blog.infrastructure.persistence.repository.AccountRepository;
-import com.raponi.blog.presentation.errors.AccountNotFound;
 import com.raponi.blog.presentation.protocols.Http;
 
 @Service
 public class FindAccountByUsernameService implements FindAccountByUsernameUseCase {
 
   private final AccountRepository accountRepository;
+  private final AccountValidatorService accountValidatorService;
 
-  public FindAccountByUsernameService(AccountRepository accountRepository) {
+  public FindAccountByUsernameService(AccountRepository accountRepository,
+      AccountValidatorService accountValidatorService) {
     this.accountRepository = accountRepository;
+    this.accountValidatorService = accountValidatorService;
   }
 
   @Override
   public Http.ResponseBody handle(String username) {
-    Optional<Account> account = this.accountRepository.findByUsername(username);
-    if (account.isPresent()) {
-      return account.get().toResponseBody();
-    }
-    throw new AccountNotFound("username equals " + username);
+    Optional<Account> acc = this.accountRepository.findByUsername(username);
+    String role = SecurityContextHolder.getContext().getAuthentication().getAuthorities().iterator().next()
+        .getAuthority();
+    Account account = this.accountValidatorService.verifyPresenceAndActive(acc, role);
+    return account.toResponseBody();
   }
 
 }
