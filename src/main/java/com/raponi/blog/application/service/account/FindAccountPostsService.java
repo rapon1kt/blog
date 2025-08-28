798da2e01ed5874a3eb2127b05b756a01e7a4aa5
rapon1kt <shinnatensei@gmail.com>
2025-08-28 11:12:05 -0300

    feat: add account verification to find account posts service

--- src/main/java/com/raponi/blog/application/service/account/FindAccountPostsService.java
+++ src/main/java/com/raponi/blog/application/service/account/FindAccountPostsService.java
@@ -3,8 +3,11 @@ package com.raponi.blog.application.service.account;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import com.raponi.blog.application.service.AccountValidatorService;
 import com.raponi.blog.domain.model.Account;
 import com.raponi.blog.domain.model.Post;
 import com.raponi.blog.domain.usecase.account.FindAccountPostsUseCase;
@@ -16,22 +19,26 @@ public class FindAccountPostsService implements FindAccountPostsUseCase {
 
   private final PostRepository postRepository;
   private final AccountRepository accountRepository;
+  private final AccountValidatorService accountValidatorService;
 
-  public FindAccountPostsService(PostRepository postRepository, AccountRepository accountRepository) {
+  public FindAccountPostsService(PostRepository postRepository, AccountRepository accountRepository,
+      AccountValidatorService accountValidatorService) {
     this.postRepository = postRepository;
     this.accountRepository = accountRepository;
+    this.accountValidatorService = accountValidatorService;
   }
 
   @Override
-  public List<Post> handle(String accountId, String currentAccountId) {
-    Optional<Account> account = this.accountRepository.findById(accountId);
-    if (account.isPresent()) {
-      if (accountId.equals(currentAccountId)) {
-        return this.postRepository.findByAccountId(accountId);
-      }
-      return this.postRepository.findByAccountIdAndPrivateStatusFalse(accountId);
+  public List<Post> handle(String accountId) {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    String role = auth.getAuthorities().iterator().next().getAuthority();
+    String tokenId = auth.getName();
+    Optional<Account> acc = this.accountRepository.findById(accountId);
+    this.accountValidatorService.verifyPresenceAndActive(acc, role);
+    if (this.accountValidatorService.verifyAuthority(accountId, tokenId, role)) {
+      return this.postRepository.findByAccountId(accountId);
     }
-    throw new IllegalArgumentException("O usuário em questão não existe.");
+    return this.postRepository.findByAccountIdAndPrivateStatusFalse(accountId);
   }
 
 }
