64ffe79d19d8253285e2b407bcaf2ca662729e3d
rapon1kt <shinnatensei@gmail.com>
2025-08-28 13:12:42 -0300

    feat: add account validator verification to updateAccountStatusService

--- src/main/java/com/raponi/blog/application/service/account/UpdateAccountStatusService.java
+++ src/main/java/com/raponi/blog/application/service/account/UpdateAccountStatusService.java
@@ -1,31 +1,38 @@
 package com.raponi.blog.application.service.account;
 
-import java.util.Optional;
-
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import com.raponi.blog.application.service.AccountValidatorService;
 import com.raponi.blog.domain.model.Account;
 import com.raponi.blog.domain.usecase.account.ChangeStatusUseCase;
 import com.raponi.blog.infrastructure.persistence.repository.AccountRepository;
-import com.raponi.blog.presentation.errors.AccountNotFound;
 import com.raponi.blog.presentation.protocols.Http;
 
 @Service
 public class UpdateAccountStatusService implements ChangeStatusUseCase {
 
   private AccountRepository accountRepository;
+  private AccountValidatorService accountValidatorService;
 
-  public UpdateAccountStatusService(AccountRepository accountRepository) {
+  public UpdateAccountStatusService(AccountRepository accountRepository,
+      AccountValidatorService accountValidatorService) {
     this.accountRepository = accountRepository;
+    this.accountValidatorService = accountValidatorService;
   }
 
   @Override
   public Http.ResponseBody handle(String accountId) {
-    Optional<Account> account = this.accountRepository.findById(accountId);
-    if (account.isPresent()) {
-      Account updated = this.accountRepository.save(account.get().changeStatus());
-      return updated.toResponseBody();
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    String role = auth.getAuthorities().iterator().next().getAuthority();
+    String tokenId = auth.getName();
+    Boolean authorized = this.accountValidatorService.verifyAuthority(accountId, tokenId, role);
+    if (authorized) {
+      Account account = this.accountRepository.findById(accountId).get();
+      return this.accountRepository.save(account.changeStatus()).toResponseBody();
     }
-    throw new AccountNotFound("id equals " + accountId);
+    throw new AccessDeniedException("Você não tem permissão para fazer isso.");
   }
 }
