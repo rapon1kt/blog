2f94604278888ec6124e743a05e028246266d927
rapon1kt <shinnatensei@gmail.com>
2025-08-28 11:11:48 -0300

    feat: add account verification to find account likes service

--- src/main/java/com/raponi/blog/application/service/account/FindAccountLikesService.java
+++ src/main/java/com/raponi/blog/application/service/account/FindAccountLikesService.java
@@ -2,8 +2,12 @@ package com.raponi.blog.application.service.account;
 
 import java.util.List;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import com.raponi.blog.application.service.AccountValidatorService;
+import com.raponi.blog.domain.model.Account;
 import com.raponi.blog.domain.model.Like;
 import com.raponi.blog.domain.usecase.account.FindAccountLikesUseCase;
 import com.raponi.blog.infrastructure.persistence.repository.LikeRepository;
@@ -12,24 +16,20 @@ import com.raponi.blog.infrastructure.persistence.repository.LikeRepository;
 public class FindAccountLikesService implements FindAccountLikesUseCase {
 
   private final LikeRepository likeRepository;
+  private final AccountValidatorService accountValidatorService;
 
-  public FindAccountLikesService(LikeRepository likeRepository) {
+  public FindAccountLikesService(LikeRepository likeRepository, AccountValidatorService accountValidatorService) {
     this.likeRepository = likeRepository;
+    this.accountValidatorService = accountValidatorService;
   }
 
   @Override
-  public List<Like> handle(String accountId, String tokenId) {
-    if (accountId.isBlank() || accountId.isEmpty()) {
-      throw new IllegalArgumentException("O usuário em questão não existe.");
-    }
-
-    /* Then, implement option to followers or following accounts to see likes */
-    if (accountId.equals(tokenId)) {
-      return this.likeRepository.findByAccountId(accountId);
-    }
-
-    throw new IllegalArgumentException("Você não tem permissão para ver os likes dessa pessoa.");
-
+  public List<Like> handle(String accountId) {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    String tokenId = auth.getName();
+    String role = auth.getAuthorities().iterator().next().getAuthority();
+    Account account = this.accountValidatorService.getAccountByAccountId(accountId, tokenId, role);
+    return this.likeRepository.findByAccountId(account.id());
   }
 
 }
