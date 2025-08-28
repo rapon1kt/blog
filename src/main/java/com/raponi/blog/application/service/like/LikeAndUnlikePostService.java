35603f14c5db346565a4c8186c654e6f33226a6e
rapon1kt <shinnatensei@gmail.com>
2025-08-28 13:41:38 -0300

    refactor: ensure that account is present and activated

--- src/main/java/com/raponi/blog/application/service/like/LikeAndUnlikePostService.java
+++ src/main/java/com/raponi/blog/application/service/like/LikeAndUnlikePostService.java
@@ -1,27 +1,44 @@
 package com.raponi.blog.application.service.like;
 
+import java.util.Optional;
+
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import com.raponi.blog.application.service.AccountValidatorService;
+import com.raponi.blog.domain.model.Account;
 import com.raponi.blog.domain.model.Like;
 import com.raponi.blog.domain.usecase.like.LikeAndUnlikePostUseCase;
+import com.raponi.blog.infrastructure.persistence.repository.AccountRepository;
 import com.raponi.blog.infrastructure.persistence.repository.LikeRepository;
 
 @Service
 public class LikeAndUnlikePostService implements LikeAndUnlikePostUseCase {
 
   private final LikeRepository likeRepository;
+  private final AccountRepository accountRepository;
+  private final AccountValidatorService accountValidatorService;
 
-  public LikeAndUnlikePostService(LikeRepository likeRepository) {
+  public LikeAndUnlikePostService(LikeRepository likeRepository, AccountRepository accountRepository,
+      AccountValidatorService accountValidatorService) {
     this.likeRepository = likeRepository;
+    this.accountRepository = accountRepository;
+    this.accountValidatorService = accountValidatorService;
   }
 
   @Override
   public String handle(String postId, String accountId) {
-    if (this.likeRepository.existsByPostIdAndAccountId(postId, accountId)) {
-      this.likeRepository.deleteByPostIdAndAccountId(postId, accountId);
+    String role = SecurityContextHolder.getContext().getAuthentication().getAuthorities().iterator().next()
+        .getAuthority();
+
+    Optional<Account> acc = this.accountRepository.findById(accountId);
+    Account validatedAcc = this.accountValidatorService.verifyPresenceAndActive(acc, role);
+
+    if (this.likeRepository.existsByPostIdAndAccountId(postId, validatedAcc.id())) {
+      this.likeRepository.deleteByPostIdAndAccountId(postId, validatedAcc.id());
       return "Unliked!";
     }
-    Like like = Like.create(accountId, postId);
+    Like like = Like.create(validatedAcc.id(), postId);
     this.likeRepository.save(like);
     return "Liked!";
   };
