158300a4d5d5a35872bfb396ca037d8940956c17
rapon1kt <shinnatensei@gmail.com>
2025-08-28 13:12:22 -0300

    feat: add account validator verification to updateAccountService

--- src/main/java/com/raponi/blog/application/service/account/UpdateAccountService.java
+++ src/main/java/com/raponi/blog/application/service/account/UpdateAccountService.java
@@ -1,32 +1,36 @@
 package com.raponi.blog.application.service.account;
 
-import java.util.Optional;
-
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import com.raponi.blog.application.service.AccountValidatorService;
 import com.raponi.blog.domain.model.Account;
 import com.raponi.blog.domain.usecase.account.UpdateAccountUseCase;
 import com.raponi.blog.infrastructure.persistence.repository.AccountRepository;
-import com.raponi.blog.presentation.errors.AccountNotFound;
 import com.raponi.blog.presentation.protocols.Http;
 
 @Service
 public class UpdateAccountService implements UpdateAccountUseCase {
 
   private final AccountRepository accountRepository;
+  private final AccountValidatorService accountValidatorService;
 
-  public UpdateAccountService(AccountRepository accountRepository) {
+  public UpdateAccountService(AccountRepository accountRepository, AccountValidatorService accountValidatorService) {
     this.accountRepository = accountRepository;
+    this.accountValidatorService = accountValidatorService;
   }
 
   @Override
   public Http.ResponseBody handle(String accountId, String newUsername) {
-    Optional<Account> accountToUpdate = this.accountRepository.findById(accountId);
-    if (accountToUpdate.isPresent()) {
-      Account accountUpdated = accountToUpdate.get().update(newUsername);
-      this.accountRepository.save(accountUpdated);
-      return accountUpdated.toResponseBody();
-    }
-    throw new AccountNotFound("id equals " + accountId);
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    String role = auth.getAuthorities().iterator().next().getAuthority();
+    String tokenId = auth.getName();
+
+    Account accountToUpdate = this.accountValidatorService.getAccountByAccountId(accountId, tokenId, role);
+    Account accountUpdated = accountToUpdate.update(newUsername);
+
+    this.accountRepository.save(accountUpdated);
+    return accountUpdated.toResponseBody();
   }
 }
