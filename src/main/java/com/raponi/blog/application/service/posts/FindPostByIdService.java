4fc182460fa72620bd3b2af6f05bb6471fb333d5
rapon1kt <shinnatensei@gmail.com>
2025-08-29 15:01:24 -0300

    refactor: implementing validatePost method to findPostById service

--- src/main/java/com/raponi/blog/application/service/posts/FindPostByIdService.java
+++ src/main/java/com/raponi/blog/application/service/posts/FindPostByIdService.java
@@ -1,35 +1,24 @@
 package com.raponi.blog.application.service.posts;
 
-import java.util.Optional;
-
 import org.springframework.stereotype.Service;
 
+import com.raponi.blog.application.service.PostValidatorService;
 import com.raponi.blog.domain.model.Post;
 import com.raponi.blog.domain.usecase.post.FindPostByIdUseCase;
-import com.raponi.blog.infrastructure.persistence.repository.PostRepository;
+import com.raponi.blog.presentation.protocols.Http;
 
 @Service
 public class FindPostByIdService implements FindPostByIdUseCase {
 
-  private final PostRepository postRepository;
+  private final PostValidatorService postValidatorService;
 
-  public FindPostByIdService(PostRepository postRepository) {
-    this.postRepository = postRepository;
+  public FindPostByIdService(PostValidatorService postValidatorService) {
+    this.postValidatorService = postValidatorService;
   }
 
   @Override
-  public Post handle(String postId, String accountId) {
-    Optional<Post> post = this.postRepository.findById(postId);
-    if (post.isPresent()) {
-      if (post.get().privateStatus()) {
-        if (post.get().accountId().equals(accountId)) {
-          return this.postRepository.findById(postId).get();
-        }
-        throw new IllegalArgumentException("Você não tem permissão para visualizar esse post.");
-      }
-      return this.postRepository.findById(postId).get();
-    }
-    throw new IllegalArgumentException("Post não encontrado ou não existe.");
+  public Http.PostResponseBody handle(String postId) {
+    Post post = this.postValidatorService.validatePostPresenceAndPrivate(postId);
+    return post.toResponseBody();
   }
-
 }
