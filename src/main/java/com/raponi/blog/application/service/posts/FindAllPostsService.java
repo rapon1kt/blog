b5971396038cd482bdcdfdd89a8228f4a89bd365
rapon1kt <shinnatensei@gmail.com>
2025-08-29 15:00:30 -0300

    refactor: admin verification to the findAll posts service

--- src/main/java/com/raponi/blog/application/service/posts/FindAllPostsService.java
+++ src/main/java/com/raponi/blog/application/service/posts/FindAllPostsService.java
@@ -2,7 +2,7 @@ package com.raponi.blog.application.service.posts;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
-
+import com.raponi.blog.application.service.AccountValidatorService;
 import com.raponi.blog.domain.model.Post;
 import com.raponi.blog.domain.usecase.post.FindAllPostsUseCase;
 import com.raponi.blog.infrastructure.persistence.repository.PostRepository;
@@ -11,15 +11,21 @@ import com.raponi.blog.presentation.protocols.Http;
 @Service
 public class FindAllPostsService implements FindAllPostsUseCase {
 
+  private final AccountValidatorService accountValidatorService;
+
   private final PostRepository postRepository;
 
-  public FindAllPostsService(PostRepository postRepository) {
+  public FindAllPostsService(PostRepository postRepository, AccountValidatorService accountValidatorService) {
     this.postRepository = postRepository;
+    this.accountValidatorService = accountValidatorService;
   }
 
   @Override
   public List<Http.PostResponseBody> handle() {
-    return this.postRepository.findByPrivateStatus(false).stream().map(Post::toResponseBody).toList();
+    if (this.accountValidatorService.isAdmin())
+      return this.postRepository.findAll().stream().map(Post::toResponseBody).toList();
+    else
+      return this.postRepository.findByPrivateStatus(false).stream().map(Post::toResponseBody).toList();
   };
 
 }
