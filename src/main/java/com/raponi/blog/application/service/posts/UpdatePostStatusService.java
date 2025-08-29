d818abfedab6916cb02def0707ad555550691fd3
rapon1kt <shinnatensei@gmail.com>
2025-08-29 15:02:56 -0300

    refactor: implementing post validation and implementing account authorization

--- src/main/java/com/raponi/blog/application/service/posts/UpdatePostStatusService.java
+++ src/main/java/com/raponi/blog/application/service/posts/UpdatePostStatusService.java
@@ -1,36 +1,42 @@
 package com.raponi.blog.application.service.posts;
 
-import java.util.Optional;
-
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
-
+import com.raponi.blog.application.service.AccountValidatorService;
+import com.raponi.blog.application.service.PostValidatorService;
 import com.raponi.blog.domain.model.Post;
 import com.raponi.blog.domain.usecase.post.UpdatePostStatusUseCase;
 import com.raponi.blog.infrastructure.persistence.repository.PostRepository;
+import com.raponi.blog.presentation.protocols.Http;
 
 @Service
 public class UpdatePostStatusService implements UpdatePostStatusUseCase {
 
   private final PostRepository postRepository;
+  private final PostValidatorService postValidatorService;
+  private final AccountValidatorService accountValidatorService;
 
-  public UpdatePostStatusService(PostRepository postRepository) {
+  public UpdatePostStatusService(PostRepository postRepository, PostValidatorService postValidatorService,
+      AccountValidatorService accountValidatorService) {
     this.postRepository = postRepository;
+    this.postValidatorService = postValidatorService;
+    this.accountValidatorService = accountValidatorService;
   }
 
   @Override
-  public Post handle(String accountId, String postId, String tokenId) {
-    Optional<Post> post = this.postRepository.findById(postId);
-    if (post.isPresent()) {
-      if (post.get().accountId().equals(accountId) && accountId.equals(tokenId)) {
-        boolean status = post.get().privateStatus();
-        boolean newStatus = status == true ? false : true;
-        Post updatedPost = post.get().changeStatus(newStatus);
-        this.postRepository.save(updatedPost);
-        return updatedPost;
-      }
-      throw new IllegalArgumentException("Você não tem permissão para fazer isso.");
+  public Http.PostResponseBody handle(String accountId, String postId) {
+    Post post = this.postValidatorService.validatePostPresenceAndPrivate(postId);
+    if (!post.accountId().equals(accountId)) {
+      throw new IllegalArgumentException("Esse usuário não é o autor desse post!");
     }
-    throw new IllegalArgumentException("O post em questão não pode ser encontrado.");
+    Boolean authorized = this.accountValidatorService.verifyAuthority(accountId);
+    if (authorized) {
+      boolean newStatus = post.privateStatus() == true ? false : true;
+      Post updatedPost = post.changeStatus(newStatus);
+      this.postRepository.save(updatedPost);
+      return updatedPost.toResponseBody();
+    }
+    throw new AccessDeniedException("Você não tem permissão para fazer isso.");
   }
 
 }
