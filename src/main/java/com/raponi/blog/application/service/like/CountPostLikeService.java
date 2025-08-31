7b4f0bd67b056e0fada9c4018afd7f0edd73bbe6
rapon1kt <shinnatensei@gmail.com>
2025-08-31 09:11:20 -0300

    feat: add post validation to countPostLike service

--- src/main/java/com/raponi/blog/application/service/like/CountPostLikeService.java
+++ src/main/java/com/raponi/blog/application/service/like/CountPostLikeService.java
@@ -2,6 +2,8 @@ package com.raponi.blog.application.service.like;
 
 import org.springframework.stereotype.Service;
 
+import com.raponi.blog.application.service.PostValidatorService;
+import com.raponi.blog.domain.model.Post;
 import com.raponi.blog.domain.usecase.like.CountPostLikeUseCase;
 import com.raponi.blog.infrastructure.persistence.repository.LikeRepository;
 
@@ -9,14 +11,17 @@ import com.raponi.blog.infrastructure.persistence.repository.LikeRepository;
 public class CountPostLikeService implements CountPostLikeUseCase {
 
   private final LikeRepository likeRepository;
+  private final PostValidatorService postValidatorService;
 
-  public CountPostLikeService(LikeRepository likeRepository) {
+  public CountPostLikeService(LikeRepository likeRepository, PostValidatorService postValidatorService) {
     this.likeRepository = likeRepository;
+    this.postValidatorService = postValidatorService;
   }
 
   @Override
   public long handle(String postId) {
-    return this.likeRepository.countByPostId(postId);
+    Post verifiedPost = this.postValidatorService.validatePostPresenceAndPrivate(postId);
+    return this.likeRepository.countByPostId(verifiedPost.id());
   }
 
 }
