907dc4ab5dcadb6a0f650d4ce9fc3fceff803f00
rapon1kt <shinnatensei@gmail.com>
2025-08-31 20:36:57 -0300

    feat: implementing comment repository

---
+++ src/main/java/com/raponi/blog/infrastructure/persistence/repository/CommentRepository.java
@@ -0,0 +1,19 @@
+package com.raponi.blog.infrastructure.persistence.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.raponi.blog.domain.model.Comment;
+
+public interface CommentRepository extends MongoRepository<Comment, String> {
+
+  List<Comment> existsByPostId(String postId);
+
+  List<Comment> existsByAccountId(String accountId);
+
+  void deleteByAccountId(String accountId);
+
+  void deleteByPostId(String postId);
+
+}
