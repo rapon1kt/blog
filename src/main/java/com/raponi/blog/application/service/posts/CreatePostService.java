b4bec3d57a97c635ba03dfeb08ca569a0661329b
rapon1kt <shinnatensei@gmail.com>
2025-08-31 09:24:49 -0300

    feat: implementing account validation to create post service

--- src/main/java/com/raponi/blog/application/service/posts/CreatePostService.java
+++ src/main/java/com/raponi/blog/application/service/posts/CreatePostService.java
@@ -2,6 +2,8 @@ package com.raponi.blog.application.service.posts;
 
 import org.springframework.stereotype.Service;
 
+import com.raponi.blog.application.service.AccountValidatorService;
+import com.raponi.blog.domain.model.Account;
 import com.raponi.blog.domain.model.Post;
 import com.raponi.blog.domain.usecase.post.CreatePostUseCase;
 import com.raponi.blog.infrastructure.persistence.repository.PostRepository;
@@ -11,14 +13,17 @@ import com.raponi.blog.presentation.protocols.Http;
 public class CreatePostService implements CreatePostUseCase {
 
   private final PostRepository postRepository;
+  private final AccountValidatorService accountValidatorService;
 
-  public CreatePostService(PostRepository postRepository) {
+  public CreatePostService(PostRepository postRepository, AccountValidatorService accountValidatorService) {
     this.postRepository = postRepository;
+    this.accountValidatorService = accountValidatorService;
   }
 
   @Override
   public Http.PostResponseBody handle(Post newPost, String tokenId) {
-    Post post = Post.create(tokenId, newPost.title(), newPost.content());
+    Account account = this.accountValidatorService.getAccountByAccountId(tokenId);
+    Post post = Post.create(account.id(), newPost.title(), newPost.content());
     this.postRepository.save(post);
     return post.toResponseBody();
   }
