f020205b86114020802f77922d0b7a11ca96052e
rapon1kt <shinnatensei@gmail.com>
2025-09-02 12:09:09 -0300

    feat: implementing create account request dto on sign up controller

--- src/main/java/com/raponi/blog/presentation/controller/SignUpController.java
+++ src/main/java/com/raponi/blog/presentation/controller/SignUpController.java
@@ -1,11 +1,13 @@
 package com.raponi.blog.presentation.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.raponi.blog.application.service.account.CreateAccountService;
-import com.raponi.blog.presentation.protocols.Http;
+import com.raponi.blog.presentation.dto.CreateAccountRequestDTO;
+import com.raponi.blog.presentation.mapper.AccountMapper;
+
+import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,12 +15,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 public class SignUpController {
 
-  @Autowired
-  private CreateAccountService createAccountService;
+  private final CreateAccountService createAccountService;
+
+  public SignUpController(CreateAccountService createAccountService, AccountMapper accountMapper) {
+    this.createAccountService = createAccountService;
+  }
 
   @PostMapping(value = "/req/signup", consumes = "application/json")
-  public ResponseEntity<?> signUp(@RequestBody Http.RegisterBody requestBody) {
-    return ResponseEntity.ok(this.createAccountService.handle(requestBody));
+  public ResponseEntity<?> signUp(@RequestBody @Valid CreateAccountRequestDTO request) {
+    return ResponseEntity.status(201).body(this.createAccountService.handle(request));
   }
 
 }
