09202e34bf8923d086251e1a052d99298fcbd207
rapon1kt <shinnatensei@gmail.com>
2025-09-02 12:15:36 -0300

    feat: implementing dtos request and response to login account service and use case

--- src/main/java/com/raponi/blog/application/service/account/LoginAccountService.java
+++ src/main/java/com/raponi/blog/application/service/account/LoginAccountService.java
@@ -9,8 +9,8 @@ import com.raponi.blog.application.service.JWTService;
 import com.raponi.blog.domain.model.Account;
 import com.raponi.blog.domain.usecase.account.LoginAccountUseCase;
 import com.raponi.blog.infrastructure.persistence.repository.AccountRepository;
+import com.raponi.blog.presentation.dto.LoginAccountRequestDTO;
 import com.raponi.blog.presentation.errors.ResourceNotFoundException;
-import com.raponi.blog.presentation.protocols.Http;
 
 @Service
 public class LoginAccountService implements LoginAccountUseCase {
@@ -27,12 +27,12 @@ public class LoginAccountService implements LoginAccountUseCase {
   }
 
   @Override
-  public String handle(Http.LoginBody bodyRequest) {
+  public String handle(LoginAccountRequestDTO requestDTO) {
     Authentication authentication = this.authenticationManager
-        .authenticate(new UsernamePasswordAuthenticationToken(bodyRequest.username(), bodyRequest.password()));
+        .authenticate(new UsernamePasswordAuthenticationToken(requestDTO.getUsername(), requestDTO.getPassword()));
     if (!authentication.isAuthenticated())
       throw new ResourceNotFoundException("This account cannot be found.");
-    Account account = this.accountRepository.findByUsername(bodyRequest.username()).get();
-    return jwtService.generateToken(bodyRequest.username(), account);
+    Account account = this.accountRepository.findByUsername(requestDTO.getUsername()).get();
+    return jwtService.generateToken(requestDTO.getUsername(), account);
   }
 }
--- src/main/java/com/raponi/blog/domain/usecase/account/LoginAccountUseCase.java
+++ src/main/java/com/raponi/blog/domain/usecase/account/LoginAccountUseCase.java
@@ -1,7 +1,7 @@
 package com.raponi.blog.domain.usecase.account;
 
-import com.raponi.blog.presentation.protocols.Http;
+import com.raponi.blog.presentation.dto.LoginAccountRequestDTO;
 
 public interface LoginAccountUseCase {
-  public String handle(Http.LoginBody bodyRequest);
+  public String handle(LoginAccountRequestDTO requestDTO);
 }
