66b80c203a63f283c8b9bb1f6cbf47784cd5f2c5
rapon1kt <shinnatensei@gmail.com>
2025-09-03 15:44:59 -0300

    refactor: removing dependency of http protocol and remove method toResponse

--- src/main/java/com/raponi/blog/domain/model/Comment.java
+++ src/main/java/com/raponi/blog/domain/model/Comment.java
@@ -2,8 +2,6 @@ package com.raponi.blog.domain.model;
 
 import java.time.Instant;
 
-import com.raponi.blog.presentation.protocols.Http;
-
 public record Comment(
     String id,
     String content,
@@ -17,7 +15,4 @@ public record Comment(
     return new Comment(null, content, accountId, postId, instant, instant);
   }
 
-  public Http.CommentResponseBody toResponseBody() {
-    return new Http.CommentResponseBody(id, content, accountId, postId, createdAt);
-  }
 }
