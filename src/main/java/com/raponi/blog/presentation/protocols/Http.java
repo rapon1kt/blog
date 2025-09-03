c33069e689e537419efc5e08aac3d29d12657156
rapon1kt <shinnatensei@gmail.com>
2025-09-03 15:11:50 -0300

    refactor: removing postResponseBody from http protocol

--- src/main/java/com/raponi/blog/presentation/protocols/Http.java
+++ src/main/java/com/raponi/blog/presentation/protocols/Http.java
@@ -2,18 +2,7 @@ package com.raponi.blog.presentation.protocols;
 
 import java.time.Instant;
 
-public record Http(PostResponseBody postResponseBody,
-    CommentResponseBody commentResponseBody) {
-
-  public record PostResponseBody(
-      String postId,
-      String title,
-      String content,
-      String accountId,
-      Instant createdAt) {
-
-  }
-
+public record Http(CommentResponseBody commentResponseBody) {
   public record CommentResponseBody(
       String id,
       String content,
