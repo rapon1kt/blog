5b47bd99230f7a482016894f01964555beb13ca0
rapon1kt <shinnatensei@gmail.com>
2025-09-04 15:13:25 -0300

    refactor: removing update account username in order to create a general update service

--- src/main/java/com/raponi/blog/domain/usecase/account/UpdateAccountUsernameUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.raponi.blog.domain.usecase.account;
-
-import com.raponi.blog.presentation.dto.AccountResponseDTO;
-import com.raponi.blog.presentation.dto.UpdateAccountUsernameRequestDTO;
-
-public interface UpdateAccountUsernameUseCase {
-  public AccountResponseDTO handle(String accountId, UpdateAccountUsernameRequestDTO requestDTO);
-}
