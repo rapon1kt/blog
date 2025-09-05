716fa62e8f57d24ff5bef4d2d065e851681c8884
rapon1kt <shinnatensei@gmail.com>
2025-09-04 23:00:52 -0300

    feat: implement to public account response dto mapper method

--- src/main/java/com/raponi/blog/presentation/mapper/AccountMapper.java
+++ src/main/java/com/raponi/blog/presentation/mapper/AccountMapper.java
@@ -5,10 +5,13 @@ import org.mapstruct.Mapper;
 import com.raponi.blog.domain.model.Account;
 import com.raponi.blog.presentation.dto.AccountResponseDTO;
 import com.raponi.blog.presentation.dto.CreatedAccountResponseDTO;
+import com.raponi.blog.presentation.dto.PublicAccountResponseDTO;
 
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
   AccountResponseDTO toResponse(Account account);
 
   CreatedAccountResponseDTO toCreatedResponse(Account account);
+
+  PublicAccountResponseDTO toPublicAccountResponseDTO(Account account);
 }
