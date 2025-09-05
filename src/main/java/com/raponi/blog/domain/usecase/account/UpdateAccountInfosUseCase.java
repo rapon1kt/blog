c598baadf4e74abf5cad72c5751207569bd75556
rapon1kt <shinnatensei@gmail.com>
2025-09-04 23:04:19 -0300

    feat: create general update account info use case

---
+++ src/main/java/com/raponi/blog/domain/usecase/account/UpdateAccountInfosUseCase.java
@@ -0,0 +1,13 @@
+package com.raponi.blog.domain.usecase.account;
+
+import java.io.IOException;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.raponi.blog.presentation.dto.AccountResponseDTO;
+import com.raponi.blog.presentation.dto.UpdateAccountInfosRequestDTO;
+
+public interface UpdateAccountInfosUseCase {
+  public AccountResponseDTO handle(String accountId, UpdateAccountInfosRequestDTO requestDTO,
+      MultipartFile image) throws IOException;
+}
