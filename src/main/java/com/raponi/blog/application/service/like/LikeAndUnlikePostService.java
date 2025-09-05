5b5bd310dbaa3106df206dff7d438656c4004e29
rapon1kt <shinnatensei@gmail.com>
2025-09-05 10:31:34 -0300

    refactor: fixing package of validators in like services

--- src/main/java/com/raponi/blog/application/service/like/LikeAndUnlikePostService.java
+++ src/main/java/com/raponi/blog/application/service/like/LikeAndUnlikePostService.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import com.raponi.blog.application.service.AccountValidatorService;
+import com.raponi.blog.application.validators.AccountValidatorService;
 import com.raponi.blog.domain.model.Account;
 import com.raponi.blog.domain.model.Like;
 import com.raponi.blog.domain.usecase.like.LikeAndUnlikePostUseCase;
