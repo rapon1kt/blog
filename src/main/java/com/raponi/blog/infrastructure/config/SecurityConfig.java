0e5bc49474ef599817f42138acae189d525a3550
rapon1kt <shinnatensei@gmail.com>
2025-09-06 19:01:07 -0300

    feat: add cors policy and register localhost in allowed origins

--- src/main/java/com/raponi/blog/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/raponi/blog/infrastructure/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.raponi.blog.infrastructure.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +18,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.raponi.blog.application.service.AppAccountServiceImpl;
 
@@ -35,6 +40,7 @@ public class SecurityConfig {
   public @Bean SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .csrf(AbstractHttpConfigurer::disable)
+        .cors(Customizer.withDefaults())
         .authorizeHttpRequests(registry -> {
           registry.requestMatchers("/req/**").permitAll();
           registry.anyRequest().authenticated();
@@ -61,6 +67,17 @@ public class SecurityConfig {
 
   public @Bean AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
     return config.getAuthenticationManager();
+  }
+
+  public @Bean CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowCredentials(true);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
 }
