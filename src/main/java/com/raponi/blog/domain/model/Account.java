003d93b7d7de9655eebb07c593ef0f8a6f499c9e
rapon1kt <shinnatensei@gmail.com>
2025-09-07 12:57:34 -0300

    refactor: changing account type to class

--- src/main/java/com/raponi/blog/domain/model/Account.java
+++ src/main/java/com/raponi/blog/domain/model/Account.java
@@ -2,55 +2,102 @@ package com.raponi.blog.domain.model;
 
 import java.time.Instant;
 
-public record Account(
-    String id,
-    String email,
-    String username,
-    String picture,
-    String description,
-    String password,
-    boolean active,
-    String role,
-    Instant createdAt,
-    Instant modifiedAt) {
+public class Account {
+  String id;
+  String email;
+  String username;
+  String picture;
+  String description;
+  String password;
+  boolean active;
+  String role;
+  Instant createdAt;
+  Instant modifiedAt;
 
-  public static Account create(String email, String username, String hashedPassword) {
-    Instant instNow = Instant.now();
-    return new Account(
-        null,
-        email,
-        username,
-        null,
-        null,
-        hashedPassword,
-        true,
-        "USER",
-        instNow,
-        instNow);
+  public Account(String email, String username, String hashedPassword) {
+    this.email = email;
+    this.username = username;
+    this.password = hashedPassword;
   }
 
-  public Account changePassword(String newPassword) {
-    return new Account(id, email, username, picture, description, newPassword, active,
-        role, createdAt, Instant.now());
+  public String getId() {
+    return id;
   }
 
-  public Account changeStatus() {
-    return new Account(id, email, username, picture, description, password,
-        active ? false : true, role, createdAt, Instant.now());
+  public void setId(String id) {
+    this.id = id;
   }
 
-  public Account updateAccountInfos(String newUsername, String newPicture, String newDescription) {
-    return new Account(
-        id,
-        email,
-        newUsername,
-        newPicture,
-        newDescription,
-        password,
-        active,
-        role,
-        createdAt,
-        Instant.now());
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPicture() {
+    return picture;
+  }
+
+  public void setPicture(String picture) {
+    this.picture = picture;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public boolean isActive() {
+    return active;
+  }
+
+  public void setActive(boolean active) {
+    this.active = active;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+
+  public Instant getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(Instant createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public Instant getModifiedAt() {
+    return modifiedAt;
+  }
+
+  public void setModifiedAt(Instant modifiedAt) {
+    this.modifiedAt = modifiedAt;
   }
 
 }
