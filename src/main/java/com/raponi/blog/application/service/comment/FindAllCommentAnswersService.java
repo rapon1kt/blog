79417a8ca2dd703082dd65a8b9573e731eb548f7
rapon1kt <shinnatensei@gmail.com>
2025-09-08 16:22:44 -0300

    feat: create find all comment answers use case and implement on a service

---
+++ src/main/java/com/raponi/blog/application/service/comment/FindAllCommentAnswersService.java
@@ -0,0 +1,29 @@
+package com.raponi.blog.application.service.comment;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.raponi.blog.domain.usecase.comment.FindAllCommentAnswersUseCase;
+import com.raponi.blog.infrastructure.persistence.repository.CommentRepository;
+import com.raponi.blog.presentation.dto.CommentResponseDTO;
+import com.raponi.blog.presentation.mapper.CommentMapper;
+
+@Service
+public class FindAllCommentAnswersService implements FindAllCommentAnswersUseCase {
+
+  private final CommentRepository commentRepository;
+  private final CommentMapper commentMapper;
+
+  public FindAllCommentAnswersService(CommentRepository commentRepository, CommentMapper commentMapper) {
+    this.commentRepository = commentRepository;
+    this.commentMapper = commentMapper;
+  }
+
+  @Override
+  public List<CommentResponseDTO> handle(String commentId) {
+    return this.commentRepository.findByCommentIdAndIsAnswerTrue(commentId).stream().map(commentMapper::toResponse)
+        .toList();
+  }
+
+}
