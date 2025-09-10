06b15ad2ba7d933d1dd3c76c1266186bb5ac760e
rapon1kt <shinnatensei@gmail.com>
2025-09-10 18:36:11 -0300

    feat: add mapping methods to convert domain and persistence entity

--- src/main/java/com/raponi/blog/presentation/mapper/AccountMapper.java
+++ src/main/java/com/raponi/blog/presentation/mapper/AccountMapper.java
@@ -3,12 +3,18 @@ package com.raponi.blog.presentation.mapper;
 import org.mapstruct.Mapper;
 
 import com.raponi.blog.domain.model.Account;
+import com.raponi.blog.infrastructure.persistence.entity.AccountEntity;
 import com.raponi.blog.presentation.dto.AccountResponseDTO;
 import com.raponi.blog.presentation.dto.CreatedAccountResponseDTO;
 import com.raponi.blog.presentation.dto.PublicAccountResponseDTO;
 
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
+
+  AccountEntity toEntity(Account account);
+
+  Account toDomain(AccountEntity accountEntity);
+
   AccountResponseDTO toResponse(Account account);
 
   CreatedAccountResponseDTO toCreatedResponse(Account account);
