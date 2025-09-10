95e1822e0e484bf02f979f64da674b449db43576
rapon1kt <shinnatensei@gmail.com>
2025-09-10 11:14:59 -0300

    refactor: add stereotype to report repository

--- src/main/java/com/raponi/blog/infrastructure/persistence/repository/ReportRepository.java
+++ src/main/java/com/raponi/blog/infrastructure/persistence/repository/ReportRepository.java
@@ -3,11 +3,13 @@ package com.raponi.blog.infrastructure.persistence.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.raponi.blog.domain.model.Report;
 import com.raponi.blog.domain.model.ReportTargetType;
 import com.raponi.blog.domain.model.ReportStatus;
 
+@Repository
 public interface ReportRepository extends MongoRepository<Report, String> {
 
   List<Report> findByTargetId(String targetId);
