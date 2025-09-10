a1fefe61bd60b40c0978f9f8456f20d7c530cb6e
rapon1kt <shinnatensei@gmail.com>
2025-09-10 11:31:53 -0300

    feat: implementing reportableRepository and adding their logic

---
+++ src/main/java/com/raponi/blog/infrastructure/persistence/repository/ReportableRepositoryImpl.java
@@ -0,0 +1,30 @@
+package com.raponi.blog.infrastructure.persistence.repository;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.raponi.blog.domain.model.ReportTargetType;
+import com.raponi.blog.domain.model.Reportable;
+import com.raponi.blog.domain.repository.ReportableRepository;
+
+@Repository
+public class ReportableRepositoryImpl implements ReportableRepository {
+
+  private final PostRepository postRepository;
+  private final CommentRepository commentRepository;
+
+  public ReportableRepositoryImpl(PostRepository postRepository, CommentRepository commentRepository) {
+    this.postRepository = postRepository;
+    this.commentRepository = commentRepository;
+  }
+
+  @Override
+  public Optional<Reportable> findById(String targetId, ReportTargetType reportTargetType) {
+    return switch (reportTargetType) {
+      case POST -> this.postRepository.findById(targetId).map(p -> (Reportable) p);
+      case COMMENT -> this.commentRepository.findById(targetId).map(c -> (Reportable) c);
+    };
+  }
+
+}
