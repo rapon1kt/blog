9b02136e27b0ec03312c5e76c2015ec751abf29f
rapon1kt <shinnatensei@gmail.com>
2025-09-11 13:02:48 -0300

    feat: add toDomain and toEntity in comment mapper

--- src/main/java/com/raponi/blog/presentation/mapper/CommentMapper.java
+++ src/main/java/com/raponi/blog/presentation/mapper/CommentMapper.java
@@ -3,9 +3,15 @@ package com.raponi.blog.presentation.mapper;
 import org.mapstruct.Mapper;
 
 import com.raponi.blog.domain.model.Comment;
+import com.raponi.blog.infrastructure.persistence.entity.CommentEntity;
 import com.raponi.blog.presentation.dto.CommentResponseDTO;
 
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
+
+  CommentEntity toEntity(Comment comment);
+
+  Comment toDomain(CommentEntity commentEntity);
+
   CommentResponseDTO toResponse(Comment comment);
 }
