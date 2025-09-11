d942332c6720e68779a3e7457b78ef27b40ad73b
rapon1kt <shinnatensei@gmail.com>
2025-09-11 12:41:10 -0300

    feat: add toDomain and toEntity methods in post mapper

--- src/main/java/com/raponi/blog/presentation/mapper/PostMapper.java
+++ src/main/java/com/raponi/blog/presentation/mapper/PostMapper.java
@@ -3,9 +3,15 @@ package com.raponi.blog.presentation.mapper;
 import org.mapstruct.Mapper;
 
 import com.raponi.blog.domain.model.Post;
+import com.raponi.blog.infrastructure.persistence.entity.PostEntity;
 import com.raponi.blog.presentation.dto.PostResponseDTO;
 
 @Mapper(componentModel = "spring")
 public interface PostMapper {
+
+  PostEntity toEntity(Post post);
+
+  Post toDomain(PostEntity postEntity);
+
   PostResponseDTO toResponse(Post post);
 }
