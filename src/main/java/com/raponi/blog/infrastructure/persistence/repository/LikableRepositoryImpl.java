d1a7a2f538975b80ae18e26c50a886bbe254dac6
rapon1kt <shinnatensei@gmail.com>
2025-09-12 15:35:57 -0300

    feat: create likable repository implementation

---
+++ src/main/java/com/raponi/blog/infrastructure/persistence/repository/LikableRepositoryImpl.java
@@ -0,0 +1,42 @@
+package com.raponi.blog.infrastructure.persistence.repository;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.raponi.blog.domain.model.Comment;
+import com.raponi.blog.domain.model.Likable;
+import com.raponi.blog.domain.model.LikeTargetType;
+import com.raponi.blog.domain.model.Post;
+import com.raponi.blog.domain.repository.CommentRepository;
+import com.raponi.blog.domain.repository.LikableRepository;
+import com.raponi.blog.domain.repository.PostRepository;
+
+@Repository
+public class LikableRepositoryImpl implements LikableRepository {
+  private final PostRepository postRepository;
+  private final CommentRepository commentRepository;
+
+  public LikableRepositoryImpl(PostRepository postRepository,
+      CommentRepository commentRepository) {
+    this.postRepository = postRepository;
+    this.commentRepository = commentRepository;
+  }
+
+  @Override
+  public Optional<Likable> findById(String id, LikeTargetType type) {
+    return switch (type) {
+      case POST -> this.postRepository.findById(id).map(p -> (Likable) p);
+      case COMMENT -> this.commentRepository.findById(id).map(c -> (Likable) c);
+    };
+  }
+
+  @Override
+  public void save(Likable likable) {
+    if (likable instanceof Post post) {
+      postRepository.save(post);
+    } else if (likable instanceof Comment comment) {
+      commentRepository.save(comment);
+    }
+  }
+}
