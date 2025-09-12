959846d029208927b0298a1d357fd9a0beb09825
rapon1kt <shinnatensei@gmail.com>
2025-09-12 11:36:19 -0300

    feat: create like mapper

---
+++ src/main/java/com/raponi/blog/presentation/mapper/LikeMapper.java
@@ -0,0 +1,15 @@
+package com.raponi.blog.presentation.mapper;
+
+import org.mapstruct.Mapper;
+
+import com.raponi.blog.domain.model.Like;
+import com.raponi.blog.infrastructure.persistence.entity.LikeEntity;
+
+@Mapper
+public interface LikeMapper {
+
+  LikeEntity toEntity(Like like);
+
+  Like toDomain(LikeEntity likeEntity);
+
+}
