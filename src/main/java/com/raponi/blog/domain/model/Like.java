21b7f8d9ac37357a750a0c7c2d439bc795bd57ae
rapon1kt <shinnatensei@gmail.com>
2025-09-12 11:45:28 -0300

    refactor: turn Like record to a class

--- src/main/java/com/raponi/blog/domain/model/Like.java
+++ src/main/java/com/raponi/blog/domain/model/Like.java
@@ -2,11 +2,18 @@ package com.raponi.blog.domain.model;
 
 import java.time.Instant;
 
-public record Like(
-    String id,
-    String accountId,
-    String postId,
-    Instant createdAt) {
+public class Like {
+  String id;
+  String accountId;
+  String postId;
+  Instant createdAt;
+
+  public Like(String id, String accountId, String postId, Instant createdAt) {
+    this.id = id;
+    this.accountId = accountId;
+    this.postId = postId;
+    this.createdAt = createdAt;
+  }
 
   public static Like create(String accountId, String postId) {
     return new Like(null, accountId, postId, Instant.now());
