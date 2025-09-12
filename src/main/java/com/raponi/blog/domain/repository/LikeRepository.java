ad95c30688b7609ad4dc7def98988086bedd0043
rapon1kt <shinnatensei@gmail.com>
2025-09-12 11:35:11 -0300

    feat: creating like repository interface on domain

---
+++ src/main/java/com/raponi/blog/domain/repository/LikeRepository.java
@@ -0,0 +1,26 @@
+package com.raponi.blog.domain.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.raponi.blog.domain.model.Like;
+
+public interface LikeRepository {
+
+  public Like save(Like like);
+
+  public Optional<Like> findById(String id);
+
+  public List<Like> findByAccountId(String accountId);
+
+  public long countByPostId(String postId);
+
+  public boolean existsByPostIdAndAccountId(String postId, String accountId);
+
+  public void deleteByPostIdAndAccountId(String postId, String accountId);
+
+  public void deleteByAccountId(String accountId);
+
+  public void deleteById(String id);
+
+}
