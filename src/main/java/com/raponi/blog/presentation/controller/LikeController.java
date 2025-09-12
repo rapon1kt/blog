c6b8ff0d2f02ba2fe9d994eadf60051a08087442
rapon1kt <shinnatensei@gmail.com>
2025-09-12 15:49:19 -0300

    refactor: implementing new services and fixing path of controller

--- src/main/java/com/raponi/blog/presentation/controller/LikeController.java
+++ src/main/java/com/raponi/blog/presentation/controller/LikeController.java
@@ -1,27 +1,41 @@
 package com.raponi.blog.presentation.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.raponi.blog.application.service.like.LikeAndUnlikePostService;
+import com.raponi.blog.application.service.like.FindTargetLikesService;
+import com.raponi.blog.application.service.like.LikeAndUnlikeService;
+import com.raponi.blog.domain.model.LikeTargetType;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-@RequestMapping("/posts/{postId}/likes")
+@RequestMapping("/likes")
 public class LikeController {
 
-  private final LikeAndUnlikePostService likeAndUnlikePostService;
+  private final LikeAndUnlikeService likeAndUnlikeService;
+  private final FindTargetLikesService findTargetLikesService;
 
-  public LikeController(LikeAndUnlikePostService likeAndUnlikePostService) {
-    this.likeAndUnlikePostService = likeAndUnlikePostService;
+  public LikeController(LikeAndUnlikeService likeAndUnlikeService, FindTargetLikesService findTargetLikesService) {
+    this.likeAndUnlikeService = likeAndUnlikeService;
+    this.findTargetLikesService = findTargetLikesService;
   }
 
-  @PostMapping
-  public ResponseEntity<?> likePost(@PathVariable("postId") String postId) {
-    return ResponseEntity.status(201).body(this.likeAndUnlikePostService.handle(postId));
+  @PostMapping("/{targetId}")
+  public ResponseEntity<?> likeAndUnlike(@PathVariable("targetId") String targetId, @RequestParam LikeTargetType type,
+      Authentication auth) {
+    return ResponseEntity.status(201).body(this.likeAndUnlikeService.handle(auth.getName(), targetId, type));
+  }
+
+  @GetMapping("/{targetId}")
+  public ResponseEntity<?> findTargetLikes(@PathVariable("targetId") String targetId,
+      @RequestParam LikeTargetType type) {
+    return ResponseEntity.ok(this.findTargetLikesService.handle(targetId, type));
   }
 
 }
