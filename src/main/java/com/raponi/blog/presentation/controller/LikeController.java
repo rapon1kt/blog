4da2c2f2c7c2e2eff5f95f9e31ec0b37a1aa8f1f
rapon1kt <shinnatensei@gmail.com>
2025-09-12 14:19:06 -0300

    refactor: removing count like service from controller

--- src/main/java/com/raponi/blog/presentation/controller/LikeController.java
+++ src/main/java/com/raponi/blog/presentation/controller/LikeController.java
@@ -3,34 +3,25 @@ package com.raponi.blog.presentation.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.raponi.blog.application.service.like.CountPostLikeService;
 import com.raponi.blog.application.service.like.LikeAndUnlikePostService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/posts/{postId}/likes")
 public class LikeController {
 
   private final LikeAndUnlikePostService likeAndUnlikePostService;
-  private final CountPostLikeService countPostLikeService;
 
-  public LikeController(LikeAndUnlikePostService likeAndUnlikePostService, CountPostLikeService countPostLikeService) {
+  public LikeController(LikeAndUnlikePostService likeAndUnlikePostService) {
     this.likeAndUnlikePostService = likeAndUnlikePostService;
-    this.countPostLikeService = countPostLikeService;
   }
 
   @PostMapping
   public ResponseEntity<?> likePost(@PathVariable("postId") String postId) {
     return ResponseEntity.status(201).body(this.likeAndUnlikePostService.handle(postId));
-  }
-
-  @GetMapping
-  public ResponseEntity<?> countPostLikes(@PathVariable("postId") String postId) {
-    return ResponseEntity.ok(this.countPostLikeService.handle(postId));
   }
 
 }
