3d64a88d65476db62ca3518c277f0b78a58a5ee5
rapon1kt <shinnatensei@gmail.com>
2025-09-15 15:30:00 -0300

    refactor: removing publicy accessible services from account controller

--- src/main/java/com/raponi/blog/presentation/controller/AccountController.java
+++ src/main/java/com/raponi/blog/presentation/controller/AccountController.java
@@ -27,38 +27,26 @@ public class AccountController {
   private FindAllAccountsService findAllService;
   private FindAccountByIdService findAccountByIdService;
   private FindAccountByEmailService findAccountByEmailService;
-  private FindAccountByUsernameService findAccountByUsernameService;
   private UpdateAccountInfosService updateAccountInfosService;
   private DeleteAccountService deleteAccountService;
   private ChangeAccountPasswordService changeAccountPasswordService;
   private UpdateAccountStatusService updateAccountStatusService;
-  private FindAccountPostsService findAccountPostsService;
   private FindAccountLikesService findAccountLikesService;
-  private FindAccountFollowersService findAccountFollowersService;
-  private final FindAccountFollowingService findAccountFollowingService;
-  private final FindAccountPicturesService findAccountPicturesService;
 
   public AccountController(FindAllAccountsService findAllService, FindAccountByIdService findAccountByIdService,
       FindAccountByEmailService findAccountByEmailService,
-      FindAccountByUsernameService findAccountByUsernameService,
       UpdateAccountInfosService updateAccountInfosService, DeleteAccountService deleteAccountService,
       ChangeAccountPasswordService changeAccountPasswordService,
-      UpdateAccountStatusService updateAccountStatusService, FindAccountPostsService findAccountPostsService,
-      FindAccountLikesService findAccountLikesService, FindAccountFollowersService findAccountFollowersService,
-      FindAccountFollowingService findAccountFollowingService, FindAccountPicturesService findAccountPicturesService) {
+      UpdateAccountStatusService updateAccountStatusService,
+      FindAccountLikesService findAccountLikesService) {
     this.findAllService = findAllService;
     this.findAccountByIdService = findAccountByIdService;
     this.findAccountByEmailService = findAccountByEmailService;
-    this.findAccountByUsernameService = findAccountByUsernameService;
     this.updateAccountInfosService = updateAccountInfosService;
     this.deleteAccountService = deleteAccountService;
     this.changeAccountPasswordService = changeAccountPasswordService;
     this.updateAccountStatusService = updateAccountStatusService;
-    this.findAccountPostsService = findAccountPostsService;
     this.findAccountLikesService = findAccountLikesService;
-    this.findAccountFollowersService = findAccountFollowersService;
-    this.findAccountFollowingService = findAccountFollowingService;
-    this.findAccountPicturesService = findAccountPicturesService;
   }
 
   @GetMapping
@@ -89,11 +77,6 @@ public class AccountController {
     return ResponseEntity.ok(this.findAccountByEmailService.handle(email));
   }
 
-  @GetMapping("/username/{username}")
-  public ResponseEntity<?> getAccountByUsername(@PathVariable("username") String username) {
-    return ResponseEntity.ok(this.findAccountByUsernameService.handle(username));
-  }
-
   @PutMapping("/{accountId}/newpassword")
   public ResponseEntity<?> changeAccountPassword(@PathVariable("accountId") String accountId,
       @RequestBody @Valid UpdateAccountPasswordRequestDTO requestDTO) {
@@ -105,28 +88,8 @@ public class AccountController {
     return ResponseEntity.ok(this.updateAccountStatusService.handle(accountId));
   }
 
-  @GetMapping("/{accountId}/posts")
-  public ResponseEntity<?> getAccountPosts(@PathVariable("accountId") String accountId) {
-    return ResponseEntity.ok(this.findAccountPostsService.handle(accountId));
-  }
-
   @GetMapping("/{accountId}/likes")
   public ResponseEntity<?> getAccountLikes(@PathVariable("accountId") String accountId) {
     return ResponseEntity.ok(this.findAccountLikesService.handle(accountId));
-  }
-
-  @GetMapping("/{accountId}/followers")
-  public ResponseEntity<?> getAccountFollowers(@PathVariable("accountId") String accountId) {
-    return ResponseEntity.ok(this.findAccountFollowersService.handle(accountId));
-  }
-
-  @GetMapping("/{accountId}/following")
-  public ResponseEntity<?> getAccountFollowing(@PathVariable("accountId") String accountId) {
-    return ResponseEntity.ok(this.findAccountFollowingService.handle(accountId));
-  }
-
-  @GetMapping("/{accountId}/picture")
-  public ResponseEntity<?> getAccountPicture(@PathVariable("accountId") String accountId) throws IOException {
-    return ResponseEntity.ok(this.findAccountPicturesService.handle(accountId));
   }
 }
