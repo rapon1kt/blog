f9b855daa0cee7008b5796b138b370dea9c56500
rapon1kt <shinnatensei@gmail.com>
2025-09-15 15:42:18 -0300

    refactor: removing public services from post controller

--- src/main/java/com/raponi/blog/presentation/controller/PostController.java
+++ src/main/java/com/raponi/blog/presentation/controller/PostController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
@@ -22,17 +21,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class PostController {
 
   private final CreatePostService createPostService;
-  private final FindAllPostsService findAllPostsService;
-  private final FindPostByIdService findPostByIdService;
   private final UpdatePostStatusService updatePostStatusService;
   private final DeletePostService deletePostService;
 
-  public PostController(CreatePostService createPostService, FindAllPostsService findAllPostsService,
-      DeletePostService deletePostService, FindPostByIdService findPostByIdService,
+  public PostController(CreatePostService createPostService,
+      DeletePostService deletePostService,
       UpdatePostStatusService updatePostStatusService) {
     this.createPostService = createPostService;
-    this.findAllPostsService = findAllPostsService;
-    this.findPostByIdService = findPostByIdService;
     this.deletePostService = deletePostService;
     this.updatePostStatusService = updatePostStatusService;
   }
@@ -40,16 +35,6 @@ public class PostController {
   @PostMapping
   public ResponseEntity<?> createPost(@RequestBody @Valid CreatePostRequestDTO requestDTO, Authentication auth) {
     return ResponseEntity.status(201).body(this.createPostService.handle(requestDTO, auth.getName()));
-  }
-
-  @GetMapping
-  public ResponseEntity<?> getAllPosts() {
-    return ResponseEntity.ok(this.findAllPostsService.handle());
-  }
-
-  @GetMapping("/{postId}")
-  public ResponseEntity<?> findPostById(@PathVariable("postId") String postId) {
-    return ResponseEntity.ok(this.findPostByIdService.handle(postId));
   }
 
   @PutMapping("/{accountId}/{postId}")
