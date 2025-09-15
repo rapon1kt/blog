21940b830c0de3172c0f778eccf7fc9e4a07fd81
rapon1kt <shinnatensei@gmail.com>
2025-09-15 15:42:24 -0300

    refactor: removing public services from comment controller

--- src/main/java/com/raponi/blog/presentation/controller/CommentController.java
+++ src/main/java/com/raponi/blog/presentation/controller/CommentController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping(path = "/posts/{postId}/comments")
@@ -22,18 +21,12 @@ public class CommentController {
 
   private final CreateCommentService createCommentService;
   private final AnswerCommentService answerCommentService;
-  private final FindAllCommentAnswersService findAllCommentAnswersService;
-  private final FindPostCommentsService findAllCommentsService;
   private final DeleteCommentService deleteCommentService;
 
   public CommentController(CreateCommentService createCommentService, AnswerCommentService answerCommentService,
-      FindAllCommentAnswersService findAllCommentAnswersService,
-      FindPostCommentsService findAllCommentsService,
       DeleteCommentService deleteCommentService) {
     this.createCommentService = createCommentService;
     this.answerCommentService = answerCommentService;
-    this.findAllCommentAnswersService = findAllCommentAnswersService;
-    this.findAllCommentsService = findAllCommentsService;
     this.deleteCommentService = deleteCommentService;
   }
 
@@ -48,16 +41,6 @@ public class CommentController {
   public ResponseEntity<?> answerComment(@PathVariable("commentId") String commentId,
       @RequestBody @Valid CreateCommentRequestDTO requestDTO, Authentication auth) {
     return ResponseEntity.status(201).body(this.answerCommentService.handle(auth.getName(), commentId, requestDTO));
-  }
-
-  @GetMapping("/{commentId}")
-  public ResponseEntity<?> getCommentAnswers(@PathVariable("commentId") String commentId) {
-    return ResponseEntity.ok(this.findAllCommentAnswersService.handle(commentId));
-  }
-
-  @GetMapping
-  public ResponseEntity<?> getPostComments(@PathVariable("postId") String postId) {
-    return ResponseEntity.ok(this.findAllCommentsService.handle(postId));
   }
 
   @DeleteMapping("/{commentId}")
