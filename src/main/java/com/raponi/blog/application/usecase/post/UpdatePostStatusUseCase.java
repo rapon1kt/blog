709afd6a5d3f383aa5540b59e1b375b850422cc2
rapon1kt <shinnatensei@gmail.com>
2025-09-16 11:50:12 -0300

    feat: add post visibility to update post use case method

--- src/main/java/com/raponi/blog/application/usecase/post/UpdatePostStatusUseCase.java
+++ src/main/java/com/raponi/blog/application/usecase/post/UpdatePostStatusUseCase.java
@@ -1,7 +1,8 @@
 package com.raponi.blog.application.usecase.post;
 
+import com.raponi.blog.domain.model.PostVisibility;
 import com.raponi.blog.presentation.dto.PostResponseDTO;
 
 public interface UpdatePostStatusUseCase {
-  public PostResponseDTO handle(String accountId, String postId);
+  public PostResponseDTO handle(String accountId, String postId, PostVisibility newVisibility);
 }
