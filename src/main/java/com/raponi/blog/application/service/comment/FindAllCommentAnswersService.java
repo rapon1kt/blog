8c828fc25ba3a473d3b80b3817c94dbb5834b408
rapon1kt <shinnatensei@gmail.com>
2025-09-20 23:52:17 -0300

    feat: add non block filter to comments services

--- src/main/java/com/raponi/blog/application/service/comment/FindAllCommentAnswersService.java
+++ src/main/java/com/raponi/blog/application/service/comment/FindAllCommentAnswersService.java
@@ -1,11 +1,14 @@
 package com.raponi.blog.application.service.comment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.raponi.blog.application.usecase.comment.FindAllCommentAnswersUseCase;
+import com.raponi.blog.application.validators.AccountValidatorService;
 import com.raponi.blog.application.validators.CommentValidatorService;
+import com.raponi.blog.domain.model.Comment;
 import com.raponi.blog.domain.repository.CommentRepository;
 import com.raponi.blog.presentation.dto.CommentResponseDTO;
 import com.raponi.blog.presentation.errors.ResourceNotFoundException;
@@ -14,15 +17,18 @@ import com.raponi.blog.presentation.mapper.CommentMapper;
 @Service
 public class FindAllCommentAnswersService implements FindAllCommentAnswersUseCase {
 
+  private final AccountValidatorService accountValidatorService;
+
   private final CommentRepository commentRepository;
   private final CommentMapper commentMapper;
   private final CommentValidatorService commentValidatorService;
 
   public FindAllCommentAnswersService(CommentRepository commentRepository, CommentMapper commentMapper,
-      CommentValidatorService commentValidatorService) {
+      CommentValidatorService commentValidatorService, AccountValidatorService accountValidatorService) {
     this.commentRepository = commentRepository;
     this.commentMapper = commentMapper;
     this.commentValidatorService = commentValidatorService;
+    this.accountValidatorService = accountValidatorService;
   }
 
   @Override
@@ -31,8 +37,18 @@ public class FindAllCommentAnswersService implements FindAllCommentAnswersUseCas
     if (!isValidComment)
       throw new ResourceNotFoundException("This comment cannot be found");
 
-    return this.commentRepository.findByCommentIdAndAnswerTrue(commentId).stream().map(commentMapper::toResponse)
-        .toList();
+    if (this.accountValidatorService.isAdmin()) {
+      return this.commentRepository.findByCommentIdAndAnswerTrue(commentId).stream().map(commentMapper::toResponse)
+          .toList();
+    } else {
+      List<Comment> answers = this.commentRepository.findByCommentIdAndAnswerTrue(commentId);
+      List<Comment> answersOfNonBlocked = new ArrayList<Comment>();
+      answers.forEach(comment -> {
+        if (!this.accountValidatorService.isBlocked(comment.getAuthorId()))
+          answersOfNonBlocked.add(comment);
+      });
+      return answersOfNonBlocked.stream().map(commentMapper::toResponse).toList();
+    }
   }
 
 }
--- src/main/java/com/raponi/blog/application/service/comment/FindPostCommentsService.java
+++ src/main/java/com/raponi/blog/application/service/comment/FindPostCommentsService.java
@@ -1,11 +1,14 @@
 package com.raponi.blog.application.service.comment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.raponi.blog.application.usecase.comment.FindPostCommentsUseCase;
+import com.raponi.blog.application.validators.AccountValidatorService;
 import com.raponi.blog.application.validators.PostValidatorService;
+import com.raponi.blog.domain.model.Comment;
 import com.raponi.blog.domain.repository.CommentRepository;
 import com.raponi.blog.presentation.dto.CommentResponseDTO;
 import com.raponi.blog.presentation.errors.ResourceNotFoundException;
@@ -17,12 +20,14 @@ public class FindPostCommentsService implements FindPostCommentsUseCase {
   private final CommentRepository commentRepository;
   private final CommentMapper commentMapper;
   private final PostValidatorService postValidatorService;
+  private final AccountValidatorService accountValidatorService;
 
   public FindPostCommentsService(CommentRepository commentRepository, CommentMapper commentMapper,
-      PostValidatorService postValidatorService) {
+      PostValidatorService postValidatorService, AccountValidatorService accountValidatorService) {
     this.commentRepository = commentRepository;
     this.commentMapper = commentMapper;
     this.postValidatorService = postValidatorService;
+    this.accountValidatorService = accountValidatorService;
   }
 
   public List<CommentResponseDTO> handle(String postId) {
@@ -31,6 +36,16 @@ public class FindPostCommentsService implements FindPostCommentsUseCase {
     if (!isValidPost)
       throw new ResourceNotFoundException("This post cannot be found");
 
-    return this.commentRepository.findByPostId(postId).stream().map(commentMapper::toResponse).toList();
+    if (this.accountValidatorService.isAdmin()) {
+      return this.commentRepository.findByPostId(postId).stream().map(commentMapper::toResponse).toList();
+    } else {
+      List<Comment> comments = this.commentRepository.findByPostId(postId);
+      List<Comment> commentsOfNonBlocked = new ArrayList<Comment>();
+      comments.forEach(comment -> {
+        if (!this.accountValidatorService.isBlocked(comment.getAuthorId()))
+          commentsOfNonBlocked.add(comment);
+      });
+      return commentsOfNonBlocked.stream().map(commentMapper::toResponse).toList();
+    }
   }
 }
