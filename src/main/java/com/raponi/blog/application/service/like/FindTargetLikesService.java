299079528f2f35b1d2e7e3955b7a5e545de48a19
rapon1kt <shinnatensei@gmail.com>
2025-09-20 23:53:43 -0300

    feat: add non block filter to find target like service

--- src/main/java/com/raponi/blog/application/service/like/FindTargetLikesService.java
+++ src/main/java/com/raponi/blog/application/service/like/FindTargetLikesService.java
@@ -1,14 +1,18 @@
 package com.raponi.blog.application.service.like;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.raponi.blog.application.usecase.like.FindTargetLikesUseCase;
+import com.raponi.blog.application.validators.AccountValidatorService;
 import com.raponi.blog.application.validators.CommentValidatorService;
 import com.raponi.blog.application.validators.PostValidatorService;
+import com.raponi.blog.domain.model.Likable;
 import com.raponi.blog.domain.model.Like;
 import com.raponi.blog.domain.model.LikeTargetType;
+import com.raponi.blog.domain.repository.LikableRepository;
 import com.raponi.blog.domain.repository.LikeRepository;
 import com.raponi.blog.presentation.errors.AccessDeniedException;
 import com.raponi.blog.presentation.errors.BusinessRuleException;
@@ -17,14 +21,19 @@ import com.raponi.blog.presentation.errors.BusinessRuleException;
 public class FindTargetLikesService implements FindTargetLikesUseCase {
 
   private final LikeRepository likeRepository;
+  private final LikableRepository likableRepository;
   private final PostValidatorService postValidatorService;
   private final CommentValidatorService commentValidatorService;
+  private final AccountValidatorService accountValidatorService;
 
-  public FindTargetLikesService(LikeRepository likeRepository, PostValidatorService postValidatorService,
-      CommentValidatorService commentValidatorService) {
+  public FindTargetLikesService(LikeRepository likeRepository, LikableRepository likableRepository,
+      PostValidatorService postValidatorService,
+      CommentValidatorService commentValidatorService, AccountValidatorService accountValidatorService) {
     this.likeRepository = likeRepository;
+    this.likableRepository = likableRepository;
     this.postValidatorService = postValidatorService;
     this.commentValidatorService = commentValidatorService;
+    this.accountValidatorService = accountValidatorService;
   }
 
   @Override
@@ -44,8 +53,19 @@ public class FindTargetLikesService implements FindTargetLikesUseCase {
       default:
         throw new BusinessRuleException("Type of content invalid");
     }
-
-    return this.likeRepository.findByTargetId(targetId);
+    Likable likable = this.likableRepository.findById(targetId, type).get();
+    boolean isViewerBlocked = this.accountValidatorService.isBlocked(likable.getAuthorId());
+    if (isViewerBlocked) {
+      return null;
+    } else {
+      List<Like> likes = this.likeRepository.findByTargetId(targetId);
+      List<Like> likesOfNonBlocked = new ArrayList<Like>();
+      likes.forEach(like -> {
+        if (!this.accountValidatorService.isBlocked(like.getAccountId()))
+          likesOfNonBlocked.add(like);
+      });
+      return likesOfNonBlocked;
+    }
   }
 
 }
