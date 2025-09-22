835d78651da3a9031345cd79f39008c4a42b463c
rapon1kt <shinnatensei@gmail.com>
2025-09-22 16:12:26 -0300

    feat: add like type to like use case params

--- src/main/java/com/raponi/blog/application/usecase/like/LikeAndUnlikeUseCase.java
+++ src/main/java/com/raponi/blog/application/usecase/like/LikeAndUnlikeUseCase.java
@@ -1,8 +1,9 @@
 package com.raponi.blog.application.usecase.like;
 
 import com.raponi.blog.domain.model.LikeTargetType;
+import com.raponi.blog.domain.model.LikeType;
 import com.raponi.blog.presentation.dto.LikeResponseDTO;
 
 public interface LikeAndUnlikeUseCase {
-  LikeResponseDTO handle(String accountId, String targetId, LikeTargetType type);
+  LikeResponseDTO handle(String accountId, String targetId, LikeType likeType, LikeTargetType type);
 }
