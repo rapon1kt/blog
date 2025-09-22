22a05206b00ff6729a107327da9331336dbc0df6
rapon1kt <shinnatensei@gmail.com>
2025-09-22 16:13:55 -0300

    feat: add logic of other types of like to likeAndUnlike service

--- src/main/java/com/raponi/blog/application/service/like/LikeAndUnlikeService.java
+++ src/main/java/com/raponi/blog/application/service/like/LikeAndUnlikeService.java
@@ -25,22 +25,29 @@ public class LikeAndUnlikeService implements LikeAndUnlikeUseCase {
   }
 
   @Override
-  public LikeResponseDTO handle(String accountId, String targetId, LikeTargetType type) {
+  public LikeResponseDTO handle(String accountId, String targetId, LikeType likeType,
+      LikeTargetType targetType) {
     boolean validatedAccount = this.accountValidatorService.verifyPresenceAndActive("_id", accountId);
     if (!validatedAccount)
       throw new AccessDeniedException("You don't have permission to do this.");
-    Likable likable = this.likableRepository.findById(targetId, type)
+    Likable likable = this.likableRepository.findById(targetId, targetType)
         .orElseThrow(() -> new ResourceNotFoundException("This resource cannot be found."));
     if (this.accountValidatorService.isBlocked(likable.getAuthorId()))
       throw new AccessDeniedException("You cannot like this content.");
     boolean isLiked = this.likeRepository.existsByTargetIdAndAccountId(targetId, accountId);
     if (!isLiked) {
-      Like like = Like.create(accountId, targetId, type);
+      Like like = Like.create(accountId, targetId, likeType, targetType);
       this.likeRepository.save(like);
       likable.incrementLikeCount();
     } else {
-      this.likeRepository.deleteByTargetIdAndAccountId(targetId, accountId);
-      likable.decrementLikeCount();
+      Like like = this.likeRepository.findByAccountIdAndTargetId(accountId, targetId).get();
+      if (likeType.equals(like.getLikeType())) {
+        this.likeRepository.deleteByTargetIdAndAccountId(targetId, accountId);
+        likable.decrementLikeCount();
+      } else {
+        this.likeRepository.deleteByTargetIdAndAccountId(targetId, accountId);
+        this.likeRepository.save(Like.create(accountId, targetId, likeType, targetType));
+      }
     }
     this.likableRepository.save(likable);
     return new LikeResponseDTO(likable.getId(), likable.getLikeCount());
