e7dc1d585fefd1c7631fe53ff04f99dc159bd0df
rapon1kt <shinnatensei@gmail.com>
2025-09-24 21:30:51 -0300

    feat: creating notification entity

---
+++ src/main/java/com/raponi/blog/infrastructure/persistence/entity/NotificationEntity.java
@@ -0,0 +1,22 @@
+package com.raponi.blog.infrastructure.persistence.entity;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.raponi.blog.domain.model.NotificationType;
+
+import lombok.Data;
+
+@Data
+@Document(collection = "notifications")
+public class NotificationEntity {
+  @Id
+  private String id;
+  private String authorId;
+  private String actorId;
+  private NotificationType type;
+  private String targetId;
+  private Instant createdAt;
+}
