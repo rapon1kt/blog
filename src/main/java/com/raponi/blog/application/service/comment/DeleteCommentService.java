2046bcda9e055d4e5e15acea4f5a7cb23218b267
rapon1kt <shinnatensei@gmail.com>
2025-09-24 22:11:55 -0300

    refactor: fixing authority logic and business in delete comment service

--- src/main/java/com/raponi/blog/application/service/comment/DeleteCommentService.java
+++ src/main/java/com/raponi/blog/application/service/comment/DeleteCommentService.java
@@ -1,13 +1,15 @@
 package com.raponi.blog.application.service.comment;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.raponi.blog.application.usecase.comment.DeleteCommentUseCase;
+import com.raponi.blog.application.validators.AccountValidatorService;
 import com.raponi.blog.application.validators.CommentValidatorService;
 import com.raponi.blog.domain.model.Comment;
 import com.raponi.blog.domain.repository.CommentRepository;
+import com.raponi.blog.presentation.errors.AccessDeniedException;
 import com.raponi.blog.presentation.errors.ResourceNotFoundException;
 
 @Service
@@ -15,25 +17,29 @@ public class DeleteCommentService implements DeleteCommentUseCase {
 
   private final CommentRepository commentRepository;
   private final CommentValidatorService commentValidatorService;
+  private final AccountValidatorService accountValidatorService;
 
-  public DeleteCommentService(CommentRepository commentRepository, CommentValidatorService commentValidatorService) {
+  public DeleteCommentService(CommentRepository commentRepository, CommentValidatorService commentValidatorService,
+      AccountValidatorService accountValidatorService) {
     this.commentRepository = commentRepository;
     this.commentValidatorService = commentValidatorService;
+    this.accountValidatorService = accountValidatorService;
   }
 
   @Override
-  public String handle(String commentId, String postId) {
-    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-    String accountId = auth.getName();
-    String role = auth.getAuthorities().iterator().next().getAuthority();
-
+  public String handle(String accountId, String commentId, String postId) {
     boolean isValidComment = this.commentValidatorService.isValidComment(commentId);
     if (isValidComment) {
-      Comment comment = this.commentRepository.findById(commentId).get();
-      if (comment.getAuthorId().equals(accountId) || role.equals("ROLE_ADMIN")) {
+      boolean isAuthorized = this.accountValidatorService.verifyAuthority("_id", accountId);
+      if (isAuthorized) {
         this.commentRepository.deleteById(commentId);
+        List<Comment> commentAnswers = this.commentRepository.findByCommentIdAndAnswerTrue(commentId);
+        commentAnswers.forEach(comment -> {
+          this.commentRepository.deleteById(comment.getId());
+        });
         return "Comment deleted with success!";
       }
+      throw new AccessDeniedException("You don't have permission to do this.");
     }
     throw new ResourceNotFoundException("This comment cannot be found.");
   }
