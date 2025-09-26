1ea408c990cf77ce8f3ddf229b77ec685e425039
Gustavo Raponi <shinnatensei@gmail.com>
2025-09-25 23:53:10 -0300

    refactor/feat: improving code organization and remove notifications of deleted comments

--- src/main/java/com/raponi/blog/application/service/comment/DeleteCommentService.java
+++ src/main/java/com/raponi/blog/application/service/comment/DeleteCommentService.java
@@ -1,7 +1,6 @@
 package com.raponi.blog.application.service.comment;
 
 import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import com.raponi.blog.application.usecase.comment.DeleteCommentUseCase;
@@ -10,6 +9,7 @@ import com.raponi.blog.application.validators.CommentValidatorService;
 import com.raponi.blog.domain.model.Comment;
 import com.raponi.blog.domain.repository.CommentRepository;
 import com.raponi.blog.domain.repository.LikeRepository;
+import com.raponi.blog.domain.repository.NotificationRepository;
 import com.raponi.blog.presentation.errors.AccessDeniedException;
 import com.raponi.blog.presentation.errors.ResourceNotFoundException;
 
@@ -18,15 +18,18 @@ public class DeleteCommentService implements DeleteCommentUseCase {
 
   private final CommentRepository commentRepository;
   private final LikeRepository likeRepository;
+  private final NotificationRepository notificationRepository;
   private final CommentValidatorService commentValidatorService;
   private final AccountValidatorService accountValidatorService;
 
   public DeleteCommentService(CommentRepository commentRepository,
       LikeRepository likeRepository,
+      NotificationRepository notificationRepository,
       CommentValidatorService commentValidatorService,
       AccountValidatorService accountValidatorService) {
     this.commentRepository = commentRepository;
     this.likeRepository = likeRepository;
+    this.notificationRepository = notificationRepository;
     this.commentValidatorService = commentValidatorService;
     this.accountValidatorService = accountValidatorService;
   }
@@ -37,18 +40,25 @@ public class DeleteCommentService implements DeleteCommentUseCase {
     if (isValidComment) {
       boolean isAuthorized = this.accountValidatorService.verifyAuthority("_id", accountId);
       if (isAuthorized) {
-        this.commentRepository.deleteById(commentId);
-        this.likeRepository.deleteByTargetId(commentId);
-        List<Comment> commentAnswers = this.commentRepository.findByCommentIdAndAnswerTrue(commentId);
-        commentAnswers.forEach(comment -> {
-          this.commentRepository.deleteById(comment.getId());
-          this.likeRepository.deleteByTargetId(comment.getId());
-        });
+        Comment comment = this.commentRepository.findById(commentId).get();
+        deleteInteractions(commentId, comment.getPostId());
         return "Comment deleted with success!";
       }
       throw new AccessDeniedException("You don't have permission to do this.");
     }
     throw new ResourceNotFoundException("This comment cannot be found.");
+  }
+
+  private void deleteInteractions(String commentId, String targetId) {
+    List<Comment> commentAnswers = this.commentRepository.findByCommentIdAndAnswerTrue(commentId);
+    commentAnswers.forEach(cmmnt -> {
+      this.commentRepository.deleteById(cmmnt.getId());
+      this.likeRepository.deleteByTargetId(cmmnt.getId());
+      this.notificationRepository.deleteByTargetId(targetId);
+    });
+    this.commentRepository.deleteById(commentId);
+    this.likeRepository.deleteByTargetId(commentId);
+    this.notificationRepository.deleteByTargetId(targetId);
   }
 
 }
