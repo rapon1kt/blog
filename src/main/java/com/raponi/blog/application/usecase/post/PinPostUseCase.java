d0b7b5696ac9935f0372f7a282ebb3cfce10710f
Gustavo Raponi <shinnatensei@gmail.com>
2025-09-28 00:02:44 -0300

    refactor/feat: changin type of response and create pinPost service

--- src/main/java/com/raponi/blog/application/usecase/post/PinPostUseCase.java
+++ src/main/java/com/raponi/blog/application/usecase/post/PinPostUseCase.java
@@ -1,5 +1,5 @@
 package com.raponi.blog.application.usecase.post;
 
 public interface PinPostUseCase {
-  public void handle(String accountId, String postId);
+  public String handle(String accountId, String postId);
 }
