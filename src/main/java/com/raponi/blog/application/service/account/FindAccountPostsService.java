116f880f988cf40df87713b2bfbaf9efdd8887f8
Gustavo Raponi <shinnatensei@gmail.com>
2025-09-29 22:44:48 -0300

    feat: adding new function 'gerAccountFeed' and implementing their logic

--- src/main/java/com/raponi/blog/application/service/account/FindAccountPostsService.java
+++ src/main/java/com/raponi/blog/application/service/account/FindAccountPostsService.java
@@ -1,5 +1,7 @@
 package com.raponi.blog.application.service.account;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.raponi.blog.application.usecase.account.FindAccountPostsUseCase;
 import com.raponi.blog.application.validators.AccountValidatorService;
 import com.raponi.blog.domain.model.Account;
+import com.raponi.blog.domain.model.Post;
 import com.raponi.blog.domain.model.PostVisibility;
 import com.raponi.blog.domain.repository.AccountRepository;
 import com.raponi.blog.domain.repository.PostRepository;
@@ -40,12 +43,33 @@ public class FindAccountPostsService implements FindAccountPostsUseCase {
     boolean isViwerBlocked = this.accountValidatorService.isBlocked(acc.getId());
     if (isViwerBlocked)
       return null;
-    if (verifiedAuthority) {
-      return this.postRepository.findByAuthorId(acc.getId()).stream().map(postMapper::toResponse).toList();
-    }
-    return this.postRepository.findByAuthorIdAndPostVisibility(acc.getId(), PostVisibility.PUBLIC).stream()
-        .map(postMapper::toResponse)
-        .toList();
+    return this.getAccountFeed(verifiedAuthority, acc.getId());
   }
 
+  private List<PostResponseDTO> getAccountFeed(boolean verifiedAuthority, String accountId) {
+    List<Post> posts = new ArrayList<>();
+    List<Post> pinned = new ArrayList<>();
+    if (verifiedAuthority) {
+      this.postRepository.findByAuthorId(accountId).forEach(post -> {
+        if (post.isPinned()) {
+          pinned.add(post);
+        } else {
+          posts.add(post);
+        }
+      });
+      posts.addAll(pinned);
+      return posts.stream().map(postMapper::toResponse).toList();
+    }
+    this.postRepository.findByPostVisibility(PostVisibility.PUBLIC).forEach(post -> {
+      if (post.isPinned()) {
+        pinned.add(post);
+      } else {
+        posts.add(post);
+      }
+    });
+    posts.addAll(pinned);
+    posts.sort(Comparator.comparing(Post::getCreatedAt).reversed());
+    posts.sort(Comparator.comparing(Post::isPinned).reversed());
+    return posts.stream().map(postMapper::toResponse).toList();
+  }
 }
