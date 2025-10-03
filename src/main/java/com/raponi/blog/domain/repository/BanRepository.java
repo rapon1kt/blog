f15aed2d5b3ec9ebce78adc84dad44750c36f69e
Gustavo Raponi <shinnatensei@gmail.com>
2025-10-02 22:58:53 -0300

    feat: adding domain ban repository

---
+++ src/main/java/com/raponi/blog/domain/repository/BanRepository.java
@@ -0,0 +1,24 @@
+package com.raponi.blog.domain.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.raponi.blog.domain.model.Ban;
+import com.raponi.blog.domain.model.BanCategory;
+import com.raponi.blog.domain.model.BanReason;
+
+public interface BanRepository {
+
+  Ban save(Ban ban);
+
+  Optional<Ban> findById(String id);
+
+  List<Ban> findByActiveTrue(boolean active);
+
+  Optional<Ban> findByBannedIdAndActiveTrue(String bannedId);
+
+  List<Ban> findByCategoryAndActiveTrue(BanCategory category);
+
+  List<Ban> findByReasonAndActiveTrue(BanReason banReason);
+
+}
