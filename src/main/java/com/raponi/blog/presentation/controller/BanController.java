b79f9a1a0fffa5d24723744b3a535f5f03425a2a
Gustavo Raponi <shinnatensei@gmail.com>
2025-10-03 16:33:09 -0300

    feat: implementing find account bans service on controller

--- src/main/java/com/raponi/blog/presentation/controller/BanController.java
+++ src/main/java/com/raponi/blog/presentation/controller/BanController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.raponi.blog.application.service.ban.BanAccountService;
+import com.raponi.blog.application.service.ban.FindAccountBansService;
 import com.raponi.blog.domain.model.BanReason;
 import com.raponi.blog.presentation.dto.BanAccountRequestDTO;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/bans")
@@ -21,15 +23,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class BanController {
 
   private final BanAccountService banAccountService;
+  private final FindAccountBansService findAccountBansService;
 
-  public BanController(BanAccountService banAccountService) {
+  public BanController(BanAccountService banAccountService, FindAccountBansService findAccountBansService) {
     this.banAccountService = banAccountService;
+    this.findAccountBansService = findAccountBansService;
   }
 
   @PostMapping("/{bannedId}")
   public ResponseEntity<?> banAccount(@PathVariable("bannedId") String bannedId,
       @RequestParam("reason") BanReason reason, Authentication auth, @RequestBody BanAccountRequestDTO requestDTO) {
     return ResponseEntity.ok(this.banAccountService.handle(auth.getName(), bannedId, reason, requestDTO));
+  }
+
+  @GetMapping("/{bannedId}")
+  public ResponseEntity<?> findAccountBans(@PathVariable("bannedId") String bannedId) {
+    return ResponseEntity.ok(this.findAccountBansService.handle(bannedId));
   }
 
 }
