8f8d050e721b0143efb23248cc4915fc391ce965
rapon1kt <shinnatensei@gmail.com>
2025-10-16 18:56:13 -0300

    test: ensure that appAccountServiceImpl throw resource not found when account is not found

---
+++ src/test/java/com/raponi/blog/application/service/AppAccountServiceImplTest.java
@@ -0,0 +1,44 @@
+package com.raponi.blog.application.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.raponi.blog.application.validators.BanValidatorService;
+import com.raponi.blog.domain.model.Account;
+import com.raponi.blog.domain.repository.AccountRepository;
+import com.raponi.blog.domain.repository.BanRepository;
+import com.raponi.blog.presentation.errors.ResourceNotFoundException;
+
+public class AppAccountServiceImplTest {
+
+  private AccountRepository accountRepository;
+  private BanRepository banRepository;
+  private BanValidatorService banValidatorService;
+  private AppAccountServiceImpl appAccountService;
+  private Account account;
+
+  @BeforeEach
+  void setup() {
+    accountRepository = mock(AccountRepository.class);
+    banRepository = mock(BanRepository.class);
+    banValidatorService = mock(BanValidatorService.class);
+    appAccountService = new AppAccountServiceImpl(accountRepository, banRepository, banValidatorService);
+    account = Account.create("email@mail.com", "username", "hashed_password");
+    account.setId("id");
+  }
+
+  @Test
+  void mustThrowWhenAccountNotFound() {
+    when(accountRepository.findByUsername("non_existent")).thenReturn(Optional.empty());
+    ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class,
+        () -> appAccountService.loadUserByUsername("non_existent"));
+    assertEquals("This account cannot be found!", exception.getMessage());
+  }
+
+}
