25c402a3b3d952f9b8a1050f2ccf8bfc6bd4a829
rapon1kt <shinnatensei@gmail.com>
2025-10-21 23:44:16 -0300

    delete: removing use cases that are not in use

--- src/main/java/com/raponi/blog/application/usecase/account/FindAllAccountsUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.raponi.blog.application.usecase.account;
-
-import java.util.List;
-
-import com.raponi.blog.presentation.dto.PublicAccountResponseDTO;
-
-public interface FindAllAccountsUseCase {
-  public List<PublicAccountResponseDTO> handle();
-}
