db34a57cd83c36232cf0a8282b40c957a0fae07a
rapon1kt <shinnatensei@gmail.com>
2026-03-11 21:07:11 -0300

    feat: creating front-end with create-next-app

---
+++ frontend/blog/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Blog",
+  description: "Created by raponikt",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistMono.className}`}>{children}</body>
+    </html>
+  );
+}
