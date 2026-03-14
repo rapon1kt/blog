b3f505d6d831c5f262646e46b8ef5404af70536c
rapon1kt <shinnatensei@gmail.com>
2026-03-14 11:29:33 -0300

    refactor: removing autowired property from security config class

--- src/main/java/com/raponi/blog/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/raponi/blog/infrastructure/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.raponi.blog.infrastructure.config;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -30,7 +29,6 @@ import com.raponi.blog.application.service.AppAccountServiceImpl;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-  @Autowired
   private final AppAccountServiceImpl appAccountServiceImpl;
   private final JWTFilter jwtFilter;
 
