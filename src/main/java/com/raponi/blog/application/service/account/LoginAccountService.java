aa76736e5fa2c1674d266638630645aae795baf6
rapon1kt <shinnatensei@gmail.com>
2026-03-14 11:31:14 -0300

    feat: implementing account mapper with new use case in login account service

--- src/main/java/com/raponi/blog/application/service/account/LoginAccountService.java
+++ src/main/java/com/raponi/blog/application/service/account/LoginAccountService.java
@@ -10,30 +10,38 @@ import com.raponi.blog.application.usecase.account.LoginAccountUseCase;
 import com.raponi.blog.domain.model.Account;
 import com.raponi.blog.domain.repository.AccountRepository;
 import com.raponi.blog.presentation.dto.LoginAccountRequestDTO;
+import com.raponi.blog.presentation.dto.LoginAccountResponseDTO;
 import com.raponi.blog.presentation.errors.ResourceNotFoundException;
+import com.raponi.blog.presentation.mapper.AccountMapper;
 
 @Service
 public class LoginAccountService implements LoginAccountUseCase {
 
+  private final AccountMapper accountMapper;
   private final AccountRepository accountRepository;
   private final AuthenticationManager authenticationManager;
   private final JWTService jwtService;
 
-  public LoginAccountService(AccountRepository accountRepository, AuthenticationManager authenticationManager,
+  public LoginAccountService(AccountMapper accountMapper, AccountRepository accountRepository,
+      AuthenticationManager authenticationManager,
       JWTService jwtService) {
+    this.accountMapper = accountMapper;
     this.accountRepository = accountRepository;
     this.authenticationManager = authenticationManager;
     this.jwtService = jwtService;
   }
 
   @Override
-  public String handle(LoginAccountRequestDTO requestDTO) {
+  public LoginAccountResponseDTO handle(LoginAccountRequestDTO requestDTO) {
     Authentication authentication = this.authenticationManager
         .authenticate(new UsernamePasswordAuthenticationToken(requestDTO.getUsername(), requestDTO.getPassword()));
     if (!authentication.isAuthenticated())
       throw new ResourceNotFoundException("This account cannot be found.");
     Account account = this.accountRepository.findByUsername(requestDTO.getUsername())
         .orElseThrow(() -> new ResourceNotFoundException("This account cannot be found."));
-    return jwtService.generateToken(requestDTO.getUsername(), account);
+    LoginAccountResponseDTO responseDTO = new LoginAccountResponseDTO();
+    responseDTO.setUser(accountMapper.toPublicAccountResponseDTO(account));
+    responseDTO.setToken(jwtService.generateToken(requestDTO.getUsername(), account));
+    return responseDTO;
   }
 }
