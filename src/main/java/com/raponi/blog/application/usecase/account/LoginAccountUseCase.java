7ca5f4002f1203665cbd12f76612a0139cad5181
rapon1kt <shinnatensei@gmail.com>
2026-03-14 11:30:36 -0300

    feat: implementing new login dto  on login account use case

--- src/main/java/com/raponi/blog/application/usecase/account/LoginAccountUseCase.java
+++ src/main/java/com/raponi/blog/application/usecase/account/LoginAccountUseCase.java
@@ -1,7 +1,8 @@
 package com.raponi.blog.application.usecase.account;
 
 import com.raponi.blog.presentation.dto.LoginAccountRequestDTO;
+import com.raponi.blog.presentation.dto.LoginAccountResponseDTO;
 
 public interface LoginAccountUseCase {
-  public String handle(LoginAccountRequestDTO requestDTO);
+  public LoginAccountResponseDTO handle(LoginAccountRequestDTO requestDTO);
 }
