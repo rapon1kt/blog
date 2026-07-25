a17c81632131a16f26af0f4d0d17f7ec7bd12f75
rapon1kt <shinnatensei@gmail.com>
2026-07-25 20:43:17 -0300

    refactor: removing method that throw NPE exception

--- src/main/java/com/raponi/blog/infrastructure/repository/PostRepositoryImpl.java
+++ src/main/java/com/raponi/blog/infrastructure/repository/PostRepositoryImpl.java
@@ -1,16 +1,14 @@
 package com.raponi.blog.infrastructure.repository;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.stereotype.Component;
-
 import com.raponi.blog.domain.model.Post;
 import com.raponi.blog.domain.model.PostVisibility;
 import com.raponi.blog.domain.repository.PostRepository;
 import com.raponi.blog.infrastructure.persistence.entity.PostEntity;
 import com.raponi.blog.infrastructure.persistence.repository.MongoPostRepository;
 import com.raponi.blog.presentation.mapper.PostMapper;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Component;
 
 @Component
 public class PostRepositoryImpl implements PostRepository {
@@ -18,7 +16,10 @@ public class PostRepositoryImpl implements PostRepository {
   private final MongoPostRepository mongoRepository;
   private final PostMapper postMapper;
 
-  public PostRepositoryImpl(MongoPostRepository mongoRepository, PostMapper postMapper) {
+  public PostRepositoryImpl(
+    MongoPostRepository mongoRepository,
+    PostMapper postMapper
+  ) {
     this.mongoRepository = mongoRepository;
     this.postMapper = postMapper;
   }
@@ -32,18 +33,25 @@ public class PostRepositoryImpl implements PostRepository {
 
   @Override
   public Optional<Post> findById(String id) {
-    Optional<PostEntity> postEntity = this.mongoRepository.findById(id);
-    return Optional.of(postEntity.map(postMapper::toDomain).orElse(null));
+    return this.mongoRepository.findById(id).map(postMapper::toDomain);
   }
 
   @Override
   public List<Post> findAll() {
-    return this.mongoRepository.findAll().stream().map(postMapper::toDomain).toList();
+    return this.mongoRepository
+      .findAll()
+      .stream()
+      .map(postMapper::toDomain)
+      .toList();
   }
 
   @Override
   public List<Post> findByAuthorId(String authorId) {
-    return this.mongoRepository.findByAuthorId(authorId).stream().map(postMapper::toDomain).toList();
+    return this.mongoRepository
+      .findByAuthorId(authorId)
+      .stream()
+      .map(postMapper::toDomain)
+      .toList();
   }
 
   @Override
@@ -53,7 +61,11 @@ public class PostRepositoryImpl implements PostRepository {
 
   @Override
   public List<Post> findByAuthorIdAndPinnedTrue(String authorId) {
-    return this.mongoRepository.findByAuthorIdAndPinnedTrue(authorId).stream().map(postMapper::toDomain).toList();
+    return this.mongoRepository
+      .findByAuthorIdAndPinnedTrue(authorId)
+      .stream()
+      .map(postMapper::toDomain)
+      .toList();
   }
 
   @Override
@@ -62,15 +74,23 @@ public class PostRepositoryImpl implements PostRepository {
   }
 
   @Override
-  public List<Post> findByAuthorIdAndPostVisibility(String authorId, PostVisibility postVisibility) {
-    return this.mongoRepository.findByAuthorIdAndPostVisibility(authorId, postVisibility).stream()
-        .map(postMapper::toDomain)
-        .toList();
+  public List<Post> findByAuthorIdAndPostVisibility(
+    String authorId,
+    PostVisibility postVisibility
+  ) {
+    return this.mongoRepository
+      .findByAuthorIdAndPostVisibility(authorId, postVisibility)
+      .stream()
+      .map(postMapper::toDomain)
+      .toList();
   }
 
   @Override
   public List<Post> findByPostVisibility(PostVisibility postVisibility) {
-    return this.mongoRepository.findByPostVisibility(postVisibility).stream().map(postMapper::toDomain).toList();
+    return this.mongoRepository
+      .findByPostVisibility(postVisibility)
+      .stream()
+      .map(postMapper::toDomain)
+      .toList();
   }
-
 }
