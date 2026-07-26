c52e155c42470a4e2b10e97cb8fac5456b058dbb
rapon1kt <shinnatensei@gmail.com>
2026-07-26 14:50:30 -0300

    refactor: improving signUpController flux

--- src/main/java/com/raponi/blog/presentation/controller/SignUpController.java
+++ src/main/java/com/raponi/blog/presentation/controller/SignUpController.java
@@ -1,29 +1,36 @@
 package com.raponi.blog.presentation.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.raponi.blog.application.service.account.CreateAccountService;
-import com.raponi.blog.presentation.dto.CreateAccountRequestDTO;
+import com.raponi.blog.presentation.dto.request.CreateAccountRequestDTO;
+import com.raponi.blog.presentation.dto.response.CreatedAccountResponseDTO;
 import com.raponi.blog.presentation.mapper.AccountMapper;
-
 import jakarta.validation.Valid;
-
+import java.net.URI;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class SignUpController {
 
   private final CreateAccountService createAccountService;
+  private final AccountMapper mapper;
 
-  public SignUpController(CreateAccountService createAccountService, AccountMapper accountMapper) {
+  public SignUpController(CreateAccountService createAccountService, AccountMapper mapper) {
     this.createAccountService = createAccountService;
+    this.mapper = mapper;
   }
 
   @PostMapping(value = "/req/signup", consumes = "application/json")
-  public ResponseEntity<?> signUp(@RequestBody @Valid CreateAccountRequestDTO request) {
-    return ResponseEntity.status(201).body(this.createAccountService.handle(request));
-  }
+  public ResponseEntity<CreatedAccountResponseDTO> signUp(@RequestBody @Valid CreateAccountRequestDTO requestDTO) {
+    var command = mapper.toCreateCommand(requestDTO);
+    var account = createAccountService.handle(command);
+    var response = mapper.toCreated(account);
 
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(response.getId()).toUri();
+
+    return ResponseEntity.created(location).body(response);
+  }
 }
