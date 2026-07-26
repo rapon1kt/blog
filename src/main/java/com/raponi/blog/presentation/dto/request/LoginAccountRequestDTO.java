637eb85aba071675eb7c97262c49b68a6a7f5425
rapon1kt <shinnatensei@gmail.com>
2026-07-26 16:24:37 -0300

    refactor: fixing requestDTO property

--- src/main/java/com/raponi/blog/presentation/dto/request/LoginAccountRequestDTO.java
+++ src/main/java/com/raponi/blog/presentation/dto/request/LoginAccountRequestDTO.java
@@ -1,23 +1,21 @@
 package com.raponi.blog.presentation.dto.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public class LoginAccountRequestDTO {
 
-  @NotBlank(message = "Email is required.")
-  @Email(message = "Email format is incorrect.")
-  private String email;
+  @NotBlank(message = "Username is required.")
+  private String username;
 
   @NotBlank(message = "Password is required.")
   private String password;
 
-  public String getEmail() {
-    return email;
+  public String getUsername() {
+    return username;
   }
 
-  public void setEmail(String email) {
-    this.email = email;
+  public void setUsername(String username) {
+    this.username = username;
   }
 
   public String getPassword() {
