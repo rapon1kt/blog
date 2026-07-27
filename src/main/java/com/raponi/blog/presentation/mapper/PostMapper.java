b27d17176400eb83dab6749f959d52f593d9271b
rapon1kt <shinnatensei@gmail.com>
2026-07-27 20:36:59 -0300

    feat: creating post presentation mapper

--- src/main/java/com/raponi/blog/presentation/mapper/PostMapper.java
+++ src/main/java/com/raponi/blog/presentation/mapper/PostMapper.java
@@ -2,16 +2,12 @@ package com.raponi.blog.presentation.mapper;
 
 import org.mapstruct.Mapper;
 
-import com.raponi.blog.domain.model.Post;
-import com.raponi.blog.infrastructure.persistence.entity.PostEntity;
-import com.raponi.blog.presentation.dto.PostResponseDTO;
+import com.raponi.blog.application.usecase.post.CreatePostCommand;
+import com.raponi.blog.presentation.dto.request.CreatePostRequestDTO;
 
 @Mapper(componentModel = "spring")
 public interface PostMapper {
 
-  PostEntity toEntity(Post post);
+  CreatePostCommand toCommand(CreatePostRequestDTO requestDTO);
 
-  Post toDomain(PostEntity postEntity);
-
-  PostResponseDTO toResponse(Post post);
 }
