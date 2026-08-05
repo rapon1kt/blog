b8b28eb5348f3a7c69e718f465aefab381fa57be
Gustavo Raponi <raponikt@gmail.com>
2026-08-05 15:19:29 -0300

    feat: creating invalidCredentialsException

---
+++ src/main/java/com/raponi/blog/domain/exception/InvalidCredentialsException.java
@@ -0,0 +1,9 @@
+package com.raponi.blog.domain.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+
+  public InvalidCredentialsException(String message) {
+    super(message);
+  }
+
+}
