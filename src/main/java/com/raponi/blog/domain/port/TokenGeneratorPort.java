44b4e2a05afed42e49aa5e90eaee27c5f17a7b95
Gustavo Raponi <raponikt@gmail.com>
2026-08-07 18:10:36 -0300

    refactor: passing account to TokenGeneratorPort

--- src/main/java/com/raponi/blog/domain/port/TokenGeneratorPort.java
+++ src/main/java/com/raponi/blog/domain/port/TokenGeneratorPort.java
@@ -1,5 +1,7 @@
 package com.raponi.blog.domain.port;
 
+import com.raponi.blog.domain.model.Account;
+
 public interface TokenGeneratorPort {
-  public String generateToken(String accountId);
+  public String generateToken(Account account);
 }
