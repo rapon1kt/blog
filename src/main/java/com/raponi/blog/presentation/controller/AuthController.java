a404972fb1e903ddc25ea41c1f8970e5cd726807
Gustavo Raponi <raponikt@gmail.com>
2026-08-07 18:13:35 -0300

    feat: implementing signIn service on AuthController

--- src/main/java/com/raponi/blog/presentation/controller/AuthController.java
+++ src/main/java/com/raponi/blog/presentation/controller/AuthController.java
@@ -7,8 +7,11 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.raponi.blog.application.service.SignInAccountService;
 import com.raponi.blog.application.service.SignUpAccountService;
+import com.raponi.blog.presentation.dto.request.SignInAccountRequestDTO;
 import com.raponi.blog.presentation.dto.request.SignUpAccountRequestDTO;
+import com.raponi.blog.presentation.dto.response.SignInAccountResponseDTO;
 import com.raponi.blog.presentation.dto.response.SignUpAccountResponseDTO;
 import com.raponi.blog.presentation.mapper.AccountPresentationMapper;
 
@@ -24,10 +27,15 @@ public class AuthController {
 
   private final AccountPresentationMapper mapper;
   private final SignUpAccountService signUpService;
+  private final SignInAccountService signInService;
 
-  public AuthController(AccountPresentationMapper mapper, SignUpAccountService signUpService) {
+  public AuthController(
+      AccountPresentationMapper mapper,
+      SignUpAccountService signUpService,
+      SignInAccountService signInService) {
     this.mapper = mapper;
     this.signUpService = signUpService;
+    this.signInService = signInService;
   }
 
   @PostMapping("/sign-up")
@@ -36,6 +44,14 @@ public class AuthController {
     var account = signUpService.handle(command);
     var response = mapper.toSignUpResponse(account);
     return ResponseEntity.status(HttpStatus.CREATED).body(response);
+  }
+
+  @PostMapping("/sign-in")
+  public ResponseEntity<SignInAccountResponseDTO> signIn(@RequestBody @Valid SignInAccountRequestDTO requestDTO) {
+    var command = mapper.toSignInCommand(requestDTO);
+    var result = signInService.handle(command);
+    var response = new SignInAccountResponseDTO(result);
+    return ResponseEntity.status(HttpStatus.OK).body(response);
   }
 
 }
