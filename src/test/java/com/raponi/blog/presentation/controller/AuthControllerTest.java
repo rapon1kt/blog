5e5a4f539e9108380bb1be91683a53cd9bdf5199
rapon1kt <shinnatensei@gmail.com>
2026-08-11 14:38:15 -0300

    feat: ensure auth controller returns correct response to client

--- src/test/java/com/raponi/blog/presentation/controller/AuthControllerTest.java
+++ src/test/java/com/raponi/blog/presentation/controller/AuthControllerTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import java.time.Instant;
 
@@ -16,11 +17,14 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.raponi.blog.application.command.SignInAccountCommand;
 import com.raponi.blog.application.command.SignUpAccountCommand;
+import com.raponi.blog.application.result.SignInAccountResult;
 import com.raponi.blog.application.usecase.SignInAccountUseCase;
 import com.raponi.blog.application.usecase.SignUpAccountUseCase;
 import com.raponi.blog.domain.model.Account;
 import com.raponi.blog.infrastructure.security.JWTAdapter;
+import com.raponi.blog.presentation.dto.request.SignInAccountRequestDTO;
 import com.raponi.blog.presentation.dto.request.SignUpAccountRequestDTO;
 import com.raponi.blog.presentation.dto.response.SignUpAccountResponseDTO;
 import com.raponi.blog.presentation.mapper.AccountPresentationMapper;
@@ -82,6 +86,42 @@ public class AuthControllerTest {
         .andExpect(jsonPath("$.id").value(mockedAccount.getId()))
         .andExpect(jsonPath("$.email").value(mockedAccount.getEmail()))
         .andExpect(jsonPath("$.username").value(mockedAccount.getUsername()));
+
+  }
+
+  @Test
+  void mustReturnCorrectDTO_WhenSignedInCorrectly() throws Exception {
+    // Creating request
+    SignInAccountRequestDTO request = new SignInAccountRequestDTO(
+        "test_email@test.com",
+        "test_password");
+
+    // Creating mocked mapper responses
+    Account mockedAccount = new Account(
+        "test_email@test.com",
+        "test_username",
+        "passwordHash");
+
+    SignInAccountCommand mockedCommand = new SignInAccountCommand(
+        "test_email@test.com",
+        "test_password");
+
+    SignInAccountResult result = new SignInAccountResult(mockedAccount, "test_jwt_token");
+
+    // Defining expected responses of mapper and service
+    when(mapper.toSignInCommand(any(SignInAccountRequestDTO.class))).thenReturn(mockedCommand);
+    when(signInUseCase.handle(any(SignInAccountCommand.class))).thenReturn(result);
+
+    mockMvc.perform(post("/auth/sign-in")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(request)))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.token").value("test_jwt_token"))
+        .andExpect(jsonPath("$.signedAccount.id").value(org.hamcrest.Matchers.nullValue()))
+        .andExpect(jsonPath("$.signedAccount.email").value("test_email@test.com"))
+        .andExpect(jsonPath("$.signedAccount.username").value("test_username"))
+        .andExpect(jsonPath("$.signedAccount.role").value("USER"));
 
   }
 
